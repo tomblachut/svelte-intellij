ae4afb084ff3efd06de8ccd900803cb848ca8dff
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-05-05 01:17:07 +0200

    Rewrite SvelteHighlightingLexer to support JS

--- src/main/java/dev/blachut/svelte/lang/SvelteHighlightingLexer.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteHighlightingLexer.kt
@@ -1,31 +1,15 @@
 package dev.blachut.svelte.lang
 
+import com.intellij.lang.javascript.JavaScriptHighlightingLexer
 import com.intellij.lang.javascript.dialects.JSLanguageLevel
 import com.intellij.lexer.HtmlHighlightingLexer
+import com.intellij.lexer.LayeredLexer
+import dev.blachut.svelte.lang.psi.SvelteTypes.CODE_FRAGMENT
+import dev.blachut.svelte.lang.psi.SvelteTypes.HTML_FRAGMENT
 
-internal class SvelteHighlightingLexer(private val jsLanguageLevel: JSLanguageLevel) : HtmlHighlightingLexer()
-
-//    override fun findScriptContentProvider(mimeType: String?): HtmlScriptContentProvider? {
-//        val provider: HtmlScriptContentProvider?
-//        //        if (mimeType != null) {
-//        //            provider = super.findScriptContentProvider(mimeType) ?: scriptContentViaLang()
-//        //        }
-//        //        else {
-//        provider = LanguageHtmlScriptContentProvider.getScriptContentProvider(jsLanguageLevel.dialect)
-//        //        }
-//        if (provider == null) {
-//            return null
-//        }
-//
-//        val moduleType = JSElementTypes.toModuleContentType(provider.scriptElementType)
-//        return if (provider.scriptElementType === moduleType) provider else object : HtmlScriptContentProvider {
-//            override fun getScriptElementType(): IElementType {
-//                return moduleType
-//            }
-//
-//            override fun getHighlightingLexer(): Lexer? {
-//                return provider.highlightingLexer
-//            }
-//        }
-//
-//    }
+class SvelteHighlightingLexer(jsLanguageLevel: JSLanguageLevel) : LayeredLexer(SvelteLexer()) {
+    init {
+        registerLayer(HtmlHighlightingLexer(), HTML_FRAGMENT)
+        registerLayer(JavaScriptHighlightingLexer(jsLanguageLevel.dialect.optionHolder), CODE_FRAGMENT)
+    }
+}
