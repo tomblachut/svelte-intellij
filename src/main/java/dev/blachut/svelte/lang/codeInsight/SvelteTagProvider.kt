09b10a32ea59afba1e338ff12443a5c8912b0c9c
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-05-05 23:12:13 +0200

    Add Svelte special tags to SvelteTagProvider

--- src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteTagProvider.kt
+++ src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteTagProvider.kt
@@ -3,14 +3,36 @@ package dev.blachut.svelte.lang.codeInsight
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.lang.html.HTMLLanguage
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.html.HtmlTag
 import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider
 import com.intellij.psi.xml.XmlTag
 import com.intellij.xml.XmlElementDescriptor
 import com.intellij.xml.XmlTagNameProvider
+import dev.blachut.svelte.lang.SvelteFileViewProvider
 import dev.blachut.svelte.lang.icons.SvelteIcons
+
+// Vue plugin uses 100, it's ok for now
+const val highPriority = 100.0
+const val mediumPriority = 50.0
+
+const val svelteNamespace = "svelte"
+const val sveltePrefix = "$svelteNamespace:"
+
+val svelteTagNames = arrayOf("self", "component", "window", "body", "head", "options")
+
+// TODO Merge with svelteBareTagLookupElements
+val svelteNamespacedTagLookupElements = svelteTagNames.map {
+    LookupElementBuilder.create(sveltePrefix + it).withIcon(SvelteIcons.FILE)
+}
+
+/**
+ * When user autocompletes after writing colon in "svelte", editor will produce i.e. "svelte:svelte:self".
+ * I'm clearly missing something, but for now, let it be.
+ */
+val svelteBareTagLookupElements = svelteTagNames.map {
+    val lookupElement = LookupElementBuilder.create(it).withIcon(SvelteIcons.FILE)
+    PrioritizedLookupElement.withPriority(lookupElement, mediumPriority)
+}
 
 /**
  * interface XmlTagNameProvider feeds data for name completion dropdown
@@ -18,7 +40,7 @@ import dev.blachut.svelte.lang.icons.SvelteIcons
  */
 class SvelteTagProvider : XmlElementDescriptorProvider, XmlTagNameProvider {
     override fun getDescriptor(tag: XmlTag?): XmlElementDescriptor? {
-        if (tag == null || tag.containingFile.language != HTMLLanguage.INSTANCE) return null
+        if (tag == null || tag.containingFile.viewProvider !is SvelteFileViewProvider) return null
 
         val file = tag.containingFile
         val visitor = SvelteScriptVisitor()
@@ -35,7 +57,18 @@ class SvelteTagProvider : XmlElementDescriptorProvider, XmlTagNameProvider {
     }
 
     override fun addTagNameVariants(elements: MutableList<LookupElement>, tag: XmlTag, namespacePrefix: String) {
-        if (!StringUtil.isEmpty(namespacePrefix) && tag !is HtmlTag) return
+        if (tag !is HtmlTag || tag.containingFile.viewProvider !is SvelteFileViewProvider) return
+
+        if (svelteNamespace == namespacePrefix) {
+            elements.addAll(svelteBareTagLookupElements)
+            // in svelte there are no custom components to scan so early return
+            return
+        } else if (!namespacePrefix.isEmpty()) {
+            // early return for namespaces other than svelte
+            return
+        } else {
+            elements.addAll(svelteNamespacedTagLookupElements)
+        }
 
         val file = tag.containingFile
         val visitor = SvelteScriptVisitor()
@@ -46,8 +79,7 @@ class SvelteTagProvider : XmlElementDescriptorProvider, XmlTagNameProvider {
         jsElement.accept(importVisitor)
         val items = importVisitor.components.map {
             val lookupElement = LookupElementBuilder.create(it).withIcon(SvelteIcons.FILE)
-            // Vue uses 100, it's ok for now
-            PrioritizedLookupElement.withPriority(lookupElement, 100.0)
+            PrioritizedLookupElement.withPriority(lookupElement, highPriority)
         }
         // TODO Link component documentation
         // TODO Include svelte internal components
@@ -55,4 +87,3 @@ class SvelteTagProvider : XmlElementDescriptorProvider, XmlTagNameProvider {
         elements.addAll(items)
     }
 }
-
