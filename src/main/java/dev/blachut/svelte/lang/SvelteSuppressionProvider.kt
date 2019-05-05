19affdd496b7ad8af0710f59db47cb3a3f915150
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-05-05 23:11:29 +0200

    Create SvelteSuppressionProvider

---
+++ src/main/java/dev/blachut/svelte/lang/SvelteSuppressionProvider.kt
@@ -0,0 +1,19 @@
+package dev.blachut.svelte.lang
+
+import com.intellij.codeInspection.XmlSuppressionProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+
+class SvelteSuppressionProvider : XmlSuppressionProvider() {
+    override fun suppressForTag(element: PsiElement, inspectionId: String) = Unit
+
+    override fun suppressForFile(element: PsiElement, inspectionId: String) = Unit
+
+    override fun isSuppressedFor(element: PsiElement, inspectionId: String): Boolean {
+        return inspectionId == "XmlUnboundNsPrefix"
+    }
+
+    override fun isProviderAvailable(file: PsiFile): Boolean {
+        return file.viewProvider is SvelteFileViewProvider
+    }
+}
