e72b83f99fb34add6b893dde6875372213276ae3
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-05-05 21:18:25 +0200

    Provide syntax highlighting for Svelte keywords

--- src/main/java/dev/blachut/svelte/lang/SvelteSyntaxHighlighter.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteSyntaxHighlighter.kt
@@ -3,9 +3,50 @@ package dev.blachut.svelte.lang
 import com.intellij.ide.highlighter.HtmlFileHighlighter
 import com.intellij.lang.javascript.dialects.JSLanguageLevel
 import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
+import com.intellij.psi.tree.IElementType
+import dev.blachut.svelte.lang.psi.SvelteTypes
 
 internal class SvelteSyntaxHighlighter(private val jsLanguageLevel: JSLanguageLevel) : HtmlFileHighlighter() {
     override fun getHighlightingLexer(): Lexer {
         return SvelteHighlightingLexer(jsLanguageLevel)
+    }
+
+    override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> {
+        return when (tokenType) {
+            SvelteTypes.START_MUSTACHE,
+            SvelteTypes.END_MUSTACHE -> SvelteSyntaxHighlighter.MUSTACHES_KEYS
+
+            SvelteTypes.COMMA,
+            SvelteTypes.START_PAREN,
+            SvelteTypes.END_PAREN,
+            SvelteTypes.IF,
+            SvelteTypes.ELSE_IF,
+            SvelteTypes.END_IF,
+            SvelteTypes.AWAIT,
+            SvelteTypes.THEN,
+            SvelteTypes.AWAIT_THEN,
+            SvelteTypes.CATCH,
+            SvelteTypes.AWAIT_END,
+            SvelteTypes.EACH,
+            SvelteTypes.AS,
+            SvelteTypes.END_EACH,
+            SvelteTypes.ELSE -> SvelteSyntaxHighlighter.KEY_KEYS
+
+            else -> super.getTokenHighlights(tokenType)
+        }
+    }
+
+    companion object {
+        private val MUSTACHES = createTextAttributesKey("SVELTE_MUSTACHES", DefaultLanguageHighlighterColors.BRACES)
+        private val KEY = createTextAttributesKey("SVELTE_KEY", DefaultLanguageHighlighterColors.KEYWORD)
+//        private val PARENS = createTextAttributesKey("SVELTE_PARENS", DefaultLanguageHighlighterColors.PARENTHESES)
+//        val COMMENT = createTextAttributesKey("SVELTE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+
+        private val MUSTACHES_KEYS = arrayOf(MUSTACHES)
+        private val KEY_KEYS = arrayOf(KEY)
+//        private val PARENS_KEYS = arrayOf(PARENS)
     }
 }
