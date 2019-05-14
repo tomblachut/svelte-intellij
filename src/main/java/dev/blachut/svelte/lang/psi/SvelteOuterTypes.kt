ec570b2cc70de1bea2c4155ae254f1395eceb64e
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-05-15 00:52:30 +0200

    Create SvelteFormattingModelBuilder

--- src/main/java/dev/blachut/svelte/lang/psi/SvelteOuterTypes.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteOuterTypes.kt
@@ -8,6 +8,6 @@ import dev.blachut.svelte.lang.psi.SvelteTypes.HTML_FRAGMENT
 
 // TODO Merge with generated SvelteTypes if possible
 object SvelteOuterTypes {
-    private val SVELTE_FRAGMENT: IElementType = SvelteElementType("SVELTE_FRAGMENT")
+    val SVELTE_FRAGMENT: IElementType = SvelteElementType("SVELTE_FRAGMENT")
     val SVELTE_HTML_TEMPLATE_DATA: IElementType = TemplateDataElementType("SVELTE_HTML_TEMPLATE_DATA", SvelteLanguage.INSTANCE, HTML_FRAGMENT, SVELTE_FRAGMENT)
 }
