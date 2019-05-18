e9dd28af4084d8904833b57af84f58c5d25e73a5
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-05-18 14:34:31 +0200

    Refactor parser

--- src/main/gen/dev/blachut/svelte/lang/psi/impl/SvelteScopeImpl.java
+++ src/main/gen/dev/blachut/svelte/lang/psi/impl/SvelteScopeImpl.java
@@ -4,8 +4,10 @@ package dev.blachut.svelte.lang.psi.impl;
 import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
+import static dev.blachut.svelte.lang.psi.SvelteTypes.*;
 import dev.blachut.svelte.lang.psi.SveltePsiElementImpl;
 import dev.blachut.svelte.lang.psi.*;
 
