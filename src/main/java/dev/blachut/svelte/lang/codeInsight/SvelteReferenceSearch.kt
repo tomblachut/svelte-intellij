fe9882ab0142127319182776c14fd584aababe53
unlocomqx <amxmodx.unloco@gmail.com>
2019-07-12 15:03:12 +0100

    check if Svelte file before searching for references use ReadAction.run to avoid read exception

--- src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteReferenceSearch.kt
+++ src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteReferenceSearch.kt
@@ -2,29 +2,34 @@ package dev.blachut.svelte.lang.codeInsight
 
 
 import com.intellij.lang.ecmascript6.psi.ES6ImportedBinding
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ReadAction
 import com.intellij.psi.PsiReference
-import com.intellij.psi.search.*
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SingleTargetRequestResultProcessor
+import com.intellij.psi.search.UsageSearchContext
 import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.util.Processor
 import com.intellij.util.QueryExecutor
+import dev.blachut.svelte.lang.SvelteFileType
 
 class SvelteReferenceSearch : QueryExecutor<PsiReference, ReferencesSearch.SearchParameters> {
     override fun execute(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor<in PsiReference>): Boolean {
-        if (!ApplicationManager.getApplication().isReadAccessAllowed) {
-            return true
-        }
-        val elementToSearch = queryParameters.elementToSearch
-        if (elementToSearch is ES6ImportedBinding) {
-            val componentName = elementToSearch.name ?: return true
-            queryParameters.optimizer.searchWord(
-                    componentName,
-                    LocalSearchScope(elementToSearch.containingFile),
-                    UsageSearchContext.IN_CODE,
-                    true,
-                    elementToSearch,
-                    SingleTargetRequestResultProcessor(elementToSearch)
-            )
+        ReadAction.run<RuntimeException> {
+            val elementToSearch = queryParameters.elementToSearch
+            val containingFile = elementToSearch.containingFile
+            if (elementToSearch is ES6ImportedBinding && containingFile.virtualFile.fileType is SvelteFileType) {
+                val componentName = elementToSearch.name
+                if (componentName != null) {
+                    queryParameters.optimizer.searchWord(
+                            componentName,
+                            LocalSearchScope(containingFile),
+                            UsageSearchContext.IN_CODE,
+                            true,
+                            elementToSearch,
+                            SingleTargetRequestResultProcessor(elementToSearch)
+                    )
+                }
+            }
         }
         return true
     }
