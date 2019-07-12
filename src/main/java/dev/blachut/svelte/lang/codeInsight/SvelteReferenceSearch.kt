1287232dfeb1b45df63c78157ef2958cc801dfd4
unlocomqx <amxmodx.unloco@gmail.com>
2019-07-12 11:50:07 +0100

    avoid read access exception when finding component usages

--- src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteReferenceSearch.kt
+++ src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteReferenceSearch.kt
@@ -2,6 +2,7 @@ package dev.blachut.svelte.lang.codeInsight
 
 
 import com.intellij.lang.ecmascript6.psi.ES6ImportedBinding
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.psi.PsiReference
 import com.intellij.psi.search.*
 import com.intellij.psi.search.searches.ReferencesSearch
@@ -10,6 +11,9 @@ import com.intellij.util.QueryExecutor
 
 class SvelteReferenceSearch : QueryExecutor<PsiReference, ReferencesSearch.SearchParameters> {
     override fun execute(queryParameters: ReferencesSearch.SearchParameters, consumer: Processor<in PsiReference>): Boolean {
+        if (!ApplicationManager.getApplication().isReadAccessAllowed) {
+            return true
+        }
         val elementToSearch = queryParameters.elementToSearch
         if (elementToSearch is ES6ImportedBinding) {
             val componentName = elementToSearch.name ?: return true
