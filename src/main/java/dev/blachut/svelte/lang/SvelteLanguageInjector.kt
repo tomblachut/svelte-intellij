09475ad667c384fac5ccfcb0e3719b937705f3a1
unlocomqx <amxmodx.unloco@gmail.com>
2019-07-22 22:46:16 +0100

    inject JS in attribute value

--- src/main/java/dev/blachut/svelte/lang/SvelteLanguageInjector.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteLanguageInjector.kt
@@ -1,16 +1,32 @@
 package dev.blachut.svelte.lang
 
 import com.intellij.lang.javascript.JavaScriptSupportLoader
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.InjectedLanguagePlaces
 import com.intellij.psi.LanguageInjector
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiLanguageInjectionHost
-import dev.blachut.svelte.lang.psi.SvelteExpression
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlAttributeValue
+import dev.blachut.svelte.lang.psi.SvelteInterpolation
 
 class SvelteLanguageInjector : LanguageInjector {
     override fun getLanguagesToInject(host: PsiLanguageInjectionHost, registrar: InjectedLanguagePlaces) {
-        if (host is SvelteExpression) {
-            registrar.addPlace(JavaScriptSupportLoader.ECMA_SCRIPT_6, TextRange.from(0, host.textLength), null, null)
+        if (host is XmlAttributeValue) {
+            injectJsInAttributeValue(host, registrar)
+        }
+    }
+
+    private fun injectJsInAttributeValue(host: PsiLanguageInjectionHost, registrar: InjectedLanguagePlaces) {
+        val attribute = (host as XmlAttributeValue)
+        val value = attribute.text
+        if (value.contains("{") && value.contains("}")) {
+            val file = PsiFileFactory.getInstance(attribute.project)
+                .createFileFromText("dummy.svelte", SvelteLanguage.INSTANCE, value, true, true)
+            val interpolations = PsiTreeUtil.findChildrenOfType(file, SvelteInterpolation::class.java)
+            for (interpolation in interpolations) {
+                registrar.addPlace(SvelteLanguage.INSTANCE, interpolation.textRange, null, null)
+                registrar.addPlace(JavaScriptSupportLoader.ECMA_SCRIPT_6, interpolation.expression.textRange, null, null)
+            }
         }
     }
 
