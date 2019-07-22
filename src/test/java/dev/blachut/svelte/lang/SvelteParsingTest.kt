0c60cde26cd040bbb101ac8584c0ceee3a8aa713
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-07-22 16:11:29 +0200

    Update SvelteParsingTest

--- src/test/java/dev/blachut/svelte/lang/SvelteParsingTest.kt
+++ src/test/java/dev/blachut/svelte/lang/SvelteParsingTest.kt
@@ -2,14 +2,15 @@ package dev.blachut.svelte.lang
 
 import com.intellij.testFramework.ParsingTestCase
 
-class SvelteParsingTest : ParsingTestCase("", "svelte", SvelteParserDefinition()) {
-    override fun getTestDataPath(): String = "src/test/resources/dev/blachut/svelte/lang"
+class SvelteParsingTest : ParsingTestCase("dev/blachut/svelte/lang", "svelte", SvelteParserDefinition()) {
+    override fun getTestDataPath(): String = "src/test/resources"
 
     fun testIfElseIf() = doTest(true)
     fun testEachAssets() = doTest(true)
     fun testExpression() = doTest(true)
     fun testIncompleteExpression() = doTest(true)
     fun testWhitespace() = doTest(true)
+    fun testNestedBlocks() = doTest(true)
 
     // TODO Improve lexer and enable following tests
 //    fun testEachAsAs() = doTest(true)
---
+++ src/test/resources/dev/blachut/svelte/lang/NestedBlocks.svelte
@@ -0,0 +1,21 @@
+<p>
+    Plain html
+</p>
+
+{#if visible}
+    <Box {name}/>
+    {#await promise then x}
+        <p>{x}<p>
+    {/await}
+{:else}
+    {@debug name}
+    <p>Kappa</p>
+{/if}
+
+<ul>
+    {#each assets as a}
+        <li>{a}</li>
+    {/each}
+</ul>
+
+
