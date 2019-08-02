a7fa356eabd12f4db240c4581d9ca236e0825bdb
unlocomqx <amxmodx.unloco@gmail.com>
2019-08-03 00:18:11 +0100

    Use resolve cache to check if variable is already declared

--- src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteReferenceContributor.kt
+++ src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteReferenceContributor.kt
@@ -1,27 +1,37 @@
 package dev.blachut.svelte.lang.codeInsight
 
-import com.intellij.lang.javascript.psi.JSDefinitionExpression
-import com.intellij.lang.javascript.psi.JSEmbeddedContent
-import com.intellij.lang.javascript.psi.JSLabeledStatement
-import com.intellij.lang.javascript.psi.JSReferenceExpression
+import com.intellij.lang.javascript.psi.*
+import com.intellij.lang.javascript.psi.impl.JSReferenceExpressionImpl
+import com.intellij.lang.javascript.psi.resolve.JSReferenceExpressionResolver
 import com.intellij.lang.javascript.psi.resolve.JSResolveResult
 import com.intellij.openapi.util.TextRange
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.*
 import com.intellij.psi.filters.ElementFilter
 import com.intellij.psi.filters.position.FilterPattern
+import com.intellij.psi.impl.source.resolve.ResolveCache
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
 
+object DeclarationFinder {
+    fun findDeclaration(element: JSReferenceExpressionImpl?): Boolean {
+        element ?: return false
+        val cachedResults = ResolveCache.getInstance(element.project).resolveWithCaching(element, JSReferenceExpressionResolver(element, false), true, false)
+        return cachedResults.find {
+            it.element is JSVariable
+        } != null
+    }
+}
 
 class SvelteReferenceContributor : PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         val pattern = PlatformPatterns.psiElement(JSReferenceExpression::class.java).and(FilterPattern(object : ElementFilter {
             override fun isAcceptable(element: Any, context: PsiElement?): Boolean {
-                if (element is JSReferenceExpression) {
-                    // TODO: check if already declared
-                    val labeledStatement = PsiTreeUtil.findFirstParent(element) { parent -> parent is JSLabeledStatement }
-                    return labeledStatement == null || !labeledStatement.text.startsWith("$:")
+                if (element is JSReferenceExpressionImpl) {
+                    if (DeclarationFinder.findDeclaration(element)) {
+                        return false
+                    }
+                    return PsiTreeUtil.findFirstParent(element) { parent -> parent is JSLabeledStatement } == null
                 }
                 return false
             }
@@ -38,16 +48,14 @@ class SvelteReferenceContributor : PsiReferenceContributor() {
     }
 }
 
-
 class SvelteLabeledReference(element: PsiElement, textRange: TextRange) : PsiReferenceBase<PsiElement>(element, textRange), PsiPolyVariantReference {
-
     override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
         val embeddedContent = PsiTreeUtil.findFirstParent(element) { psiElement -> psiElement is JSEmbeddedContent }
         val labeledStatements = PsiTreeUtil.findChildrenOfType(embeddedContent, JSLabeledStatement::class.java)
         labeledStatements.forEach {
             val definitions = PsiTreeUtil.findChildrenOfType(it, JSDefinitionExpression::class.java)
             definitions.forEach { definition ->
-                if (definition.name == element.text) {
+                if (definition.name == element.text && !DeclarationFinder.findDeclaration(PsiTreeUtil.findChildOfType(definition, JSReferenceExpressionImpl::class.java))) {
                     return arrayOf(JSResolveResult(definition))
                 }
             }
