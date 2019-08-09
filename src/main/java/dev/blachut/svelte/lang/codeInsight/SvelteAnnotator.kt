00cdccc79cf927d52994655380ee2dafcfb0be10
unlocomqx <amxmodx.unloco@gmail.com>
2019-08-09 14:45:50 +0100

    Remove JS label annotation to get expected keyword highlighting

--- src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteAnnotator.kt
+++ src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteAnnotator.kt
@@ -1,5 +1,6 @@
 package dev.blachut.svelte.lang.codeInsight
 
+import com.intellij.codeInsight.daemon.impl.AnnotationHolderImpl
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.javascript.highlighting.JSHighlighter
@@ -11,10 +12,13 @@ import dev.blachut.svelte.lang.SvelteFileViewProvider
 class SvelteAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         if (element is JSLabeledStatement && element.text.startsWith("$:") && element.containingFile.viewProvider is SvelteFileViewProvider) {
-            val start = element.textOffset
-            val range = TextRange.from(start, 1)
-            val annotation = holder.createInfoAnnotation(range, "Svelte Label")
-            annotation.textAttributes = JSHighlighter.JS_KEYWORD
+            if (holder is AnnotationHolderImpl) {
+                holder.removeIf { it.textAttributes.externalName == "JS.LABEL" }
+                val start = element.textOffset
+                val range = TextRange.from(start, 2)
+                val annotation = holder.createInfoAnnotation(range, "Svelte Label")
+                annotation.textAttributes = JSHighlighter.JS_KEYWORD
+            }
         }
     }
 }
