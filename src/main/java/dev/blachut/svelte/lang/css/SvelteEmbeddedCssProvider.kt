4421d73c58f80adb676c8fd5ecb6eb0161e85400
AleksandrSl <sl.aleksandr28@gmail.com>
2019-10-25 14:30:01 +0300

    Enable completion of css from the same svelte component

---
+++ src/main/java/dev/blachut/svelte/lang/css/SvelteEmbeddedCssProvider.kt
@@ -0,0 +1,9 @@
+package dev.blachut.svelte.lang.css
+
+import com.intellij.lang.Language
+import com.intellij.psi.css.EmbeddedCssProvider
+import dev.blachut.svelte.lang.SvelteHTMLLanguage
+
+class SvelteEmbeddedCssProvider : EmbeddedCssProvider() {
+    override fun enableEmbeddedCssFor(language: Language): Boolean = language is SvelteHTMLLanguage
+}
