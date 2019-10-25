5b9272fc1aaeda2277201ecef6b6581816e03166
AleksandrSl <sl.aleksandr28@gmail.com>
2019-10-25 17:43:48 +0300

    Enable support for different style languages through lang attribute
    Tested manually with stylus and scss

---
+++ src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlLexerHandle.kt
@@ -0,0 +1,16 @@
+package dev.blachut.svelte.lang.parsing.html
+
+import com.intellij.lexer.BaseHtmlLexer
+import com.intellij.psi.tree.IElementType
+
+// Adapted from org.jetbrains.vuejs.lang.html.lexer.VueLexerHandle
+interface SvelteHtmlLexerHandle {
+
+    fun registerHandler(elementType: IElementType, value: BaseHtmlLexer.TokenHandler)
+
+    var seenTag: Boolean
+    var seenStyleType: Boolean
+    val seenStyle: Boolean
+    val styleType: String?
+    val inTagState: Boolean
+}
