7a7d8deba06f66051056322862647a4e6f1176d5
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-10-27 23:19:20 +0100

    Handle $$props variable

--- src/main/java/dev/blachut/svelte/lang/SvelteJSAnalysisHandlersFactory.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteJSAnalysisHandlersFactory.kt
@@ -2,12 +2,21 @@
 package dev.blachut.svelte.lang
 
 import com.intellij.lang.ecmascript6.validation.ES6AnalysisHandlersFactory
+import com.intellij.lang.javascript.index.JSSymbolUtil
+import com.intellij.lang.javascript.psi.JSReferenceExpression
 import com.intellij.lang.javascript.validation.JSReferenceChecker
 import com.intellij.lang.javascript.validation.JSReferenceInspectionProblemReporter
 import com.intellij.lang.javascript.validation.TypedJSReferenceChecker
 
 class SvelteJSAnalysisHandlersFactory : ES6AnalysisHandlersFactory() {
     override fun getReferenceChecker(reporter: JSReferenceInspectionProblemReporter): JSReferenceChecker {
-        return TypedJSReferenceChecker(reporter)
+        return object : TypedJSReferenceChecker(reporter) {
+            override fun checkRefExpression(node: JSReferenceExpression) {
+                val isPropsReference = JSSymbolUtil.isAccurateReferenceExpressionName(node, "\$\$props")
+                if (!isPropsReference) {
+                    super.checkRefExpression(node)
+                }
+            }
+        }
     }
 }
