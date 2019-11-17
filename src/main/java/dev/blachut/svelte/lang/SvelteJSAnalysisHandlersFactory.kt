d2ed44df650cf63f8c155434a42cecd6000382c3
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-11-17 02:08:29 +0100

    Suppress unresolved reference errors for $-subscriptions

--- src/main/java/dev/blachut/svelte/lang/SvelteJSAnalysisHandlersFactory.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteJSAnalysisHandlersFactory.kt
@@ -2,21 +2,12 @@
 package dev.blachut.svelte.lang
 
 import com.intellij.lang.ecmascript6.validation.ES6AnalysisHandlersFactory
-import com.intellij.lang.javascript.index.JSSymbolUtil
-import com.intellij.lang.javascript.psi.JSReferenceExpression
 import com.intellij.lang.javascript.validation.JSReferenceChecker
 import com.intellij.lang.javascript.validation.JSReferenceInspectionProblemReporter
 import com.intellij.lang.javascript.validation.TypedJSReferenceChecker
 
 class SvelteJSAnalysisHandlersFactory : ES6AnalysisHandlersFactory() {
     override fun getReferenceChecker(reporter: JSReferenceInspectionProblemReporter): JSReferenceChecker {
-        return object : TypedJSReferenceChecker(reporter) {
-            override fun checkRefExpression(node: JSReferenceExpression) {
-                val isPropsReference = JSSymbolUtil.isAccurateReferenceExpressionName(node, "\$\$props")
-                if (!isPropsReference) {
-                    super.checkRefExpression(node)
-                }
-            }
-        }
+        return TypedJSReferenceChecker(reporter)
     }
 }
--- src/main/java/dev/blachut/svelte/lang/SvelteJSReferenceExpressionResolver.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteJSReferenceExpressionResolver.kt
@@ -1,9 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package dev.blachut.svelte.lang
 
+import com.intellij.lang.javascript.index.JSSymbolUtil
 import com.intellij.lang.javascript.psi.impl.JSReferenceExpressionImpl
 import com.intellij.lang.javascript.psi.resolve.JSReferenceExpressionResolver
 import com.intellij.lang.javascript.psi.resolve.JSResolveResult
+import com.intellij.lang.javascript.psi.stubs.JSImplicitElement
+import com.intellij.lang.javascript.psi.stubs.impl.JSImplicitElementImpl
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveResult
 import com.intellij.psi.util.PsiTreeUtil
@@ -13,6 +16,25 @@ class SvelteJSReferenceExpressionResolver(referenceExpression: JSReferenceExpres
                                           ignorePerformanceLimits: Boolean) :
     JSReferenceExpressionResolver(referenceExpression, ignorePerformanceLimits) {
     override fun resolve(expression: JSReferenceExpressionImpl, incompleteCode: Boolean): Array<ResolveResult> {
+        val propsReferenceName = "\$\$props"
+
+        val name = expression.referenceName
+        if (name != null && expression.qualifier == null && name.length > 1 && name[0] == '$' && name[1] != '$') {
+            val element = JSImplicitElementImpl.Builder(name, expression)
+                .forbidAstAccess()
+                .setProperties(JSImplicitElement.Property.Constant)
+                .toImplicitElement()
+            return arrayOf(JSResolveResult(element))
+        }
+
+        if (JSSymbolUtil.isAccurateReferenceExpressionName(expression, propsReferenceName)) {
+            val element = JSImplicitElementImpl.Builder(propsReferenceName, expression)
+                .forbidAstAccess()
+                .setProperties(JSImplicitElement.Property.Constant)
+                .toImplicitElement()
+            return arrayOf(JSResolveResult(element))
+        }
+
         return resolveInComponent(expression) ?: super.resolve(expression, incompleteCode)
     }
 
