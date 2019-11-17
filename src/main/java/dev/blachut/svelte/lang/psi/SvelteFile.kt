ff7f2ffc133ee39ceb58ec87f1106c855e5ca893
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-11-17 16:52:40 +0100

    Swap interfaces implemented by SvelteFile

--- src/main/java/dev/blachut/svelte/lang/psi/SvelteFile.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteFile.kt
@@ -1,20 +1,22 @@
 package dev.blachut.svelte.lang.psi
 
 import com.intellij.extapi.psi.PsiFileBase
-import com.intellij.lang.javascript.psi.JSElement
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveState
 import com.intellij.psi.scope.PsiScopeProcessor
+import com.intellij.psi.search.PsiElementProcessor
+import com.intellij.psi.xml.XmlElement
 import dev.blachut.svelte.lang.SvelteFileType
 import dev.blachut.svelte.lang.SvelteHTMLLanguage
 import dev.blachut.svelte.lang.SvelteLanguage
 
 /**
- * JSElement interface is required by com.intellij.lang.javascript.psi.resolve.JSResolveUtil.processSiblingsForElement
+ * XmlElement interface is required by com.intellij.lang.javascript.psi.resolve.JSResolveUtil.processSiblingsForElement
+ * JSElement could also be used but it breaks ES6PsiUtil.resolveDefaultExport()
  */
-class SvelteFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, SvelteLanguage.INSTANCE), JSElement {
+class SvelteFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, SvelteLanguage.INSTANCE), XmlElement {
     override fun getFileType(): FileType {
         return SvelteFileType.INSTANCE
     }
@@ -25,5 +27,9 @@ class SvelteFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, Sve
 
     override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement): Boolean {
         return viewProvider.getPsi(SvelteHTMLLanguage.INSTANCE).processDeclarations(processor, state, lastParent, place)
+    }
+
+    override fun processElements(processor: PsiElementProcessor<*>?, place: PsiElement?): Boolean {
+        return (viewProvider.getPsi(SvelteHTMLLanguage.INSTANCE) as SvelteHtmlFile).processElements(processor, place)
     }
 }
