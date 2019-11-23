5ed0b35c23896f264d49e14c5e9c18381a7dd8b2
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-11-23 18:43:46 +0100

    Create SvelteCssExpressionErrorFilter (#80)

---
+++ src/main/java/dev/blachut/svelte/lang/css/SvelteCssExpressionErrorFilter.kt
@@ -0,0 +1,20 @@
+package dev.blachut.svelte.lang.css
+
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.psi.PsiErrorElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlAttributeValue
+
+class SvelteCssExpressionErrorFilter : HighlightErrorFilter() {
+    override fun shouldHighlightErrorElement(element: PsiErrorElement): Boolean {
+        return !isSvelteExpressionSpecialCase(element)
+    }
+
+    private fun isSvelteExpressionSpecialCase(element: PsiErrorElement): Boolean {
+        if (element.errorDescription != "a term expected") return false
+        val parent = PsiTreeUtil.getParentOfType(element, XmlAttributeValue::class.java) ?: return false
+        // TODO Replace ASTWrapperPsiElement with dedicated expression type
+        return PsiTreeUtil.getChildOfType(parent, ASTWrapperPsiElement::class.java) != null
+    }
+}
