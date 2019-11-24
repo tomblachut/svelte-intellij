f404e64de1f6a31b2f9d9fa21aec128edd988411
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-11-24 13:42:42 +0100

    Parse reactive declarations & statements

--- src/main/java/dev/blachut/svelte/lang/parsing/js/SvelteJSParser.kt
+++ src/main/java/dev/blachut/svelte/lang/parsing/js/SvelteJSParser.kt
@@ -1,12 +1,16 @@
 package dev.blachut.svelte.lang.parsing.js
 
 import com.intellij.lang.PsiBuilder
+import com.intellij.lang.WhitespacesBinders
 import com.intellij.lang.ecmascript6.parsing.ES6ExpressionParser
 import com.intellij.lang.ecmascript6.parsing.ES6FunctionParser
 import com.intellij.lang.ecmascript6.parsing.ES6Parser
 import com.intellij.lang.ecmascript6.parsing.ES6StatementParser
+import com.intellij.lang.javascript.JSElementTypes
+import com.intellij.lang.javascript.JSStubElementTypes
 import com.intellij.lang.javascript.JSTokenTypes
 import com.intellij.lang.javascript.parsing.JSPsiTypeParser
+import com.intellij.lang.javascript.parsing.JavaScriptParserBase
 import com.intellij.psi.tree.IElementType
 import dev.blachut.svelte.lang.SvelteJSLanguage
 
@@ -17,6 +21,39 @@ class SvelteJSParser(builder: PsiBuilder) : ES6Parser<ES6ExpressionParser<*>, ES
             override fun getVariableElementType(): IElementType {
                 // TODO Try to crate lazy element that splits variable and $ prefix
                 return super.getVariableElementType()
+            }
+
+            override fun parseDialectSpecificSourceElements(marker: PsiBuilder.Marker): Boolean {
+                return tryParseReactiveDeclaration(marker) || super.parseDialectSpecificSourceElements(marker)
+            }
+
+            private fun tryParseReactiveDeclaration(marker: PsiBuilder.Marker): Boolean {
+                val dollarIdentifier = builder.tokenType == JSTokenTypes.IDENTIFIER && builder.tokenText == "$"
+                val reactiveLabel = dollarIdentifier && builder.lookAhead(1) === JSTokenTypes.COLON
+                // Treat reactive statements as normal labelled statements
+                val reactiveDeclaration = reactiveLabel && isIdentifierToken(builder.lookAhead(2))
+                if (!reactiveDeclaration) return false
+
+                if (isAfterModifierKeyword()) {
+                    builder.error("reactive declarations can't be exported")
+                }
+
+                val varModifierMarker = builder.mark()
+                builder.advanceLexer()
+                builder.advanceLexer()
+                varModifierMarker.collapse(JSTokenTypes.CONST_KEYWORD)
+
+                parseVarDeclaration(false)
+                forceCheckForSemicolon()
+
+                marker.done(JSStubElementTypes.VAR_STATEMENT)
+                marker.setCustomEdgeTokenBinders(JavaScriptParserBase.INCLUDE_DOC_COMMENT_AT_LEFT, WhitespacesBinders.DEFAULT_RIGHT_BINDER)
+                return true
+            }
+
+            private fun isAfterModifierKeyword(): Boolean {
+                val doneMarker = builder.latestDoneMarker ?: return false
+                return doneMarker.tokenType == JSElementTypes.ATTRIBUTE_LIST && doneMarker.textLength > 0
             }
         }
 
