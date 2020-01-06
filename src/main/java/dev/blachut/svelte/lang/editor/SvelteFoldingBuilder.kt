a6988791c469ac35bdcb72a3f58a4da9ecc8f3eb
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-01-06 13:12:09 +0100

    Rewrite SvelteFoldingBuilder

--- src/main/java/dev/blachut/svelte/lang/editor/SvelteFoldingBuilder.kt
+++ src/main/java/dev/blachut/svelte/lang/editor/SvelteFoldingBuilder.kt
@@ -5,7 +5,9 @@ import com.intellij.lang.folding.FoldingBuilder
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import dev.blachut.svelte.lang.parsing.html.psi.SvelteBlock
 import java.util.*
 
 class SvelteFoldingBuilder : FoldingBuilder, DumbAware {
@@ -18,6 +20,20 @@ class SvelteFoldingBuilder : FoldingBuilder, DumbAware {
     private fun appendDescriptors(block: PsiElement, descriptors: MutableList<FoldingDescriptor>, document: Document) {
         if (isSingleLine(block, document)) {
             return
+        }
+
+        if (block is SvelteBlock) {
+            val endTag = block.endTag
+
+            if (endTag != null) {
+                // Following offsets ensure that we fold start and end tag together exactly like HTML does
+                // E.g. {#if condition...}
+                val foldingRangeStartOffset = block.startTag.textRange.endOffset - 1
+                val foldingRangeEndOffset = endTag.textRange.endOffset - 1
+                val range = TextRange(foldingRangeStartOffset, foldingRangeEndOffset)
+
+                descriptors.add(FoldingDescriptor(block, range))
+            }
         }
 
         var child: PsiElement? = block.firstChild
