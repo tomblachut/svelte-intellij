e21f3eec1218f5fa1f13a8aee6a814b399e61cdc
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-01-06 11:53:26 +0100

    Fix SvelteJSParameter.getUseScope

--- src/main/java/dev/blachut/svelte/lang/psi/SvelteJSElementTypes.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteJSElementTypes.kt
@@ -2,13 +2,10 @@ package dev.blachut.svelte.lang.psi
 
 import com.intellij.lang.ASTNode
 import com.intellij.lang.javascript.types.JSParameterElementType
-import com.intellij.psi.PsiElement
 
 object SvelteJSElementTypes {
     val PARAMETER = object : JSParameterElementType("EMBEDDED_PARAMETER") {
-        override fun construct(node: ASTNode): PsiElement? {
-            return SvelteJSParameter(node)
-        }
+        override fun construct(node: ASTNode) = SvelteJSParameter(node)
     }
 }
 
--- src/main/java/dev/blachut/svelte/lang/psi/SvelteJSParameter.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteJSParameter.kt
@@ -4,9 +4,14 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.javascript.psi.impl.JSParameterImpl
 import com.intellij.psi.search.LocalSearchScope
 import com.intellij.psi.search.SearchScope
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlTag
+import dev.blachut.svelte.lang.parsing.html.psi.SvelteBranch
 
 class SvelteJSParameter(node: ASTNode) : JSParameterImpl(node) {
     override fun getUseScope(): SearchScope {
-        return LocalSearchScope.EMPTY
+        val tag = PsiTreeUtil.getParentOfType(this, SvelteBranch::class.java, XmlTag::class.java)
+        tag ?: return LocalSearchScope.EMPTY
+        return LocalSearchScope(tag)
     }
 }
