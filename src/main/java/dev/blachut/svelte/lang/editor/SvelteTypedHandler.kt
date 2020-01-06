f95d20fb3b15dad17cc53602d55e2ad9b49da9f1
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-01-06 12:05:22 +0100

    Rewrite SvelteTypedHandler

--- src/main/java/dev/blachut/svelte/lang/editor/SvelteTypedHandler.kt
+++ src/main/java/dev/blachut/svelte/lang/editor/SvelteTypedHandler.kt
@@ -7,11 +7,15 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import dev.blachut.svelte.lang.SvelteHTMLLanguage
 import dev.blachut.svelte.lang.SvelteLanguage
+import dev.blachut.svelte.lang.parsing.html.psi.SvelteBlock
+import dev.blachut.svelte.lang.psi.SvelteBlockLazyElementTypes
 
 /**
- * Handler for custom plugin actions on chars typed by the user.  See [SvelteEnterHandler] for custom actions
- * on Enter.
+ * Handler for custom plugin actions on chars typed by the user.
+ * See [SvelteEnterHandler] for custom actions on Enter.
  *
  * Based on Handlebars plugin
  */
@@ -33,17 +37,33 @@ class SvelteTypedHandler : TypedHandlerDelegate() {
 
         if (c == '}' && previousChar != "{") {
             PsiDocumentManager.getInstance(project).commitDocument(editor.document)
-            finishClosingTag(offset, editor, provider, true)
+            finishEndTag(offset, editor, provider, true)
         } else if (c == '/' && previousChar == "{") {
-            // TODO Remove this commit when "{#if variable}{" doesn't break parser
             PsiDocumentManager.getInstance(project).commitDocument(editor.document)
-            finishClosingTag(offset, editor, provider, false)
+            finishEndTag(offset, editor, provider, false)
         }
 
         return Result.CONTINUE
     }
 
-    private fun finishClosingTag(offset: Int, editor: Editor, provider: FileViewProvider, justAfterOpeningTag: Boolean) {
+    private fun finishEndTag(offset: Int, editor: Editor, provider: FileViewProvider, justAfterStartTag: Boolean) {
+        val elementAtCaret = provider.findElementAt(offset - 1, SvelteHTMLLanguage.INSTANCE) ?: return
+        val block = PsiTreeUtil.getParentOfType(elementAtCaret, SvelteBlock::class.java) ?: return
 
+        if (block.endTag != null) return
+
+        val prefix = if (justAfterStartTag) "{/" else ""
+
+        val matchingTag = when (block.startTag.type) {
+            SvelteBlockLazyElementTypes.IF_START -> prefix + "if}"
+            SvelteBlockLazyElementTypes.EACH_START -> prefix + "each}"
+            SvelteBlockLazyElementTypes.AWAIT_START -> prefix + "await}"
+            else -> return
+        }
+
+        editor.document.insertString(offset, matchingTag)
+        if (!justAfterStartTag) {
+            editor.caretModel.moveToOffset(offset + matchingTag.length)
+        }
     }
 }
