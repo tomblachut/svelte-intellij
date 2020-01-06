bcafde0a40f02c8faec61d9968ff467a1827d420
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-01-06 23:40:36 +0100

    Add reference resolving for let directives

--- src/main/java/dev/blachut/svelte/lang/psi/SvelteHtmlTag.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteHtmlTag.kt
@@ -1,7 +1,13 @@
 package dev.blachut.svelte.lang.psi
 
+import com.intellij.lang.javascript.psi.JSParameter
+import com.intellij.lang.javascript.psi.JSVariable
+import com.intellij.lang.javascript.psi.util.JSDestructuringVisitor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.ResolveState
 import com.intellij.psi.html.HtmlTag
 import com.intellij.psi.impl.source.xml.XmlTagImpl
+import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.IXmlTagElementType
@@ -15,6 +21,31 @@ val SVELTE_HTML_TAG = SvelteHtmlTagElementType("SVELTE_HTML_TAG")
 
 // Check XmlTagImpl.createDelegate && HtmlTagDelegate if something breaks. Esp. HtmlTagDelegate.findSubTags
 class SvelteHtmlTag : XmlTagImpl(SVELTE_HTML_TAG), HtmlTag {
+    override fun processDeclarations(processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement): Boolean {
+        for (attribute in attributes) {
+            if (!attribute.name.startsWith("let:")) continue
+            val value = attribute.valueElement ?: continue
+            val parameter = PsiTreeUtil.findChildOfType(value, SvelteJSParameter::class.java) ?: continue
+
+            var result = true
+            parameter.accept(object : JSDestructuringVisitor() {
+                override fun visitJSParameter(node: JSParameter) {
+                    if (result && !processor.execute(node, ResolveState.initial())) {
+                        result = false
+                    }
+                }
+
+                override fun visitJSVariable(node: JSVariable) {}
+            })
+
+            if (!result) {
+                return false
+            }
+        }
+
+        return true
+    }
+
     override fun isCaseSensitive(): Boolean {
         return true
     }
