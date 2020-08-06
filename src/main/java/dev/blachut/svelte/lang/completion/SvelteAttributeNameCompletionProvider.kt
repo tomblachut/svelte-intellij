edaaf94c708106f608393581a4c2cbd26c13ab0a
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-08-06 22:15:03 +0200

    Add completions for src and global attributes of style tag

--- src/main/java/dev/blachut/svelte/lang/completion/SvelteAttributeNameCompletionProvider.kt
+++ src/main/java/dev/blachut/svelte/lang/completion/SvelteAttributeNameCompletionProvider.kt
@@ -5,6 +5,7 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlAttribute
@@ -13,18 +14,39 @@ import com.intellij.util.ProcessingContext
 import dev.blachut.svelte.lang.icons.SvelteIcons
 
 class SvelteAttributeNameCompletionProvider : CompletionProvider<CompletionParameters>() {
-    override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
         val xmlTag = PsiTreeUtil.getParentOfType(parameters.position, XmlTag::class.java, false)
         val xmlAttribute = parameters.position.parent as? XmlAttribute
         if (xmlTag == null || xmlAttribute == null) return
 
         if (xmlTag.name == "script" && xmlTag.getAttribute("context") == null) {
-            val element = LookupElementBuilder
-                .create("context=\"module\"")
-                .withIcon(SvelteIcons.GRAY)
-                .let { PrioritizedLookupElement.withPriority(it, 10.0) }
-
-            result.addElement(element)
+            result.addElement(createLookupElement("context=\"module\"", 10))
         }
+
+        // TODO refactor into proper descriptors
+        if (xmlTag.name == "style" && xmlTag.getAttribute("global") == null) {
+            result.addElement(createLookupElement("global"))
+        }
+
+        if (xmlTag.name == "style" && xmlTag.getAttribute("src") == null) {
+            result.addElement(createLookupElement("src"))
+        }
+    }
+
+    private fun createLookupElement(text: String, priority: Int? = null): LookupElement {
+        return LookupElementBuilder
+            .create(text)
+            .withIcon(SvelteIcons.GRAY)
+            .let {
+                if (priority != null) {
+                    PrioritizedLookupElement.withPriority(it, priority.toDouble())
+                } else {
+                    it
+                }
+            }
     }
 }
