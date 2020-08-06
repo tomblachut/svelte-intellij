496e853379866322aa54d0aea5342d26fd8c2795
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-08-06 20:55:04 +0200

    Fix folding of empty fragments

---
+++ src/test/resources/dev/blachut/svelte/lang/editor/FoldingEmptySvelteTag.svelte
@@ -0,0 +1,5 @@
+{#if name<fold text='...' expand='true'>}<fold text='...' expand='true'>
+    <h3<fold text='...' expand='true'>>
+        <b>{name}</b>
+    </h3</fold>>
+</fold>{:else}{/if</fold>}
---
+++ src/test/resources/dev/blachut/svelte/lang/editor/FoldingSvelteTagInHtmlTag.svelte
@@ -0,0 +1,5 @@
+<div<fold text='...' expand='true'>>
+    {#if test<fold text='...' expand='true'>}<fold text='...' expand='true'>
+        {test}
+    </fold>{/if</fold>}
+</div</fold>>
