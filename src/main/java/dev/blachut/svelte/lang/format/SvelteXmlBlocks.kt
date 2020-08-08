a40b5092843f07688f79c02bf943061012181f54
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-08-08 16:24:57 +0200

    Refactor SvelteXmlBlocks

---
+++ src/main/java/dev/blachut/svelte/lang/format/SvelteXmlBlocks.kt
@@ -0,0 +1,144 @@
+package dev.blachut.svelte.lang.format
+
+import com.intellij.formatting.*
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.formatter.xml.AbstractXmlBlock
+import com.intellij.psi.formatter.xml.XmlBlock
+import com.intellij.psi.formatter.xml.XmlFormattingPolicy
+import com.intellij.psi.formatter.xml.XmlTagBlock
+import com.intellij.psi.impl.source.SourceTreeToPsiMap
+import com.intellij.psi.xml.XmlTag
+import dev.blachut.svelte.lang.psi.blocks.SvelteBlock
+
+class SvelteXmlBlock(
+    node: ASTNode?,
+    wrap: Wrap?,
+    alignment: Alignment?,
+    policy: XmlFormattingPolicy?,
+    indent: Indent?,
+    textRange: TextRange?,
+    preserveSpace: Boolean
+) : XmlBlock(node, wrap, alignment, policy, indent, textRange, preserveSpace) {
+    override fun processSimpleChild(
+        child: ASTNode,
+        indent: Indent?,
+        result: MutableList<in Block>,
+        wrap: Wrap?,
+        alignment: Alignment?
+    ) {
+        if (isSvelteBlock(child)) {
+            result.add(createTagBlock(child, indent ?: Indent.getNoneIndent(), wrap, alignment))
+        } else {
+            super.processSimpleChild(child, indent, result, wrap, alignment)
+        }
+    }
+
+    @Suppress("UnstableApiUsage")
+    override fun createSimpleChild(child: ASTNode?, indent: Indent?, wrap: Wrap?, alignment: Alignment?): XmlBlock {
+        return createSimpleChild(myXmlFormattingPolicy, child, indent, wrap, alignment)
+    }
+
+    override fun createTagBlock(child: ASTNode?, indent: Indent?, wrap: Wrap?, alignment: Alignment?): XmlTagBlock {
+        return createTagBlock(myXmlFormattingPolicy, child, indent, wrap, alignment)
+    }
+}
+
+abstract class SvelteXmlTagBlockBase(
+    node: ASTNode?,
+    wrap: Wrap?,
+    alignment: Alignment?,
+    policy: XmlFormattingPolicy?,
+    indent: Indent?,
+    preserveSpace: Boolean
+) : XmlTagBlock(node, wrap, alignment, policy, indent, preserveSpace) {
+    // start getTag related overrides
+    override fun getTag(): XmlTag {
+        return super.getTag() ?: getFakeTag(myNode)
+    }
+
+    private fun getFakeTag(node: ASTNode): XmlTag {
+        val element = SourceTreeToPsiMap.treeElementToPsi(node)
+
+        if (element is SvelteBlock) {
+            return SvelteBlockFakeXmlTag(element)
+        }
+
+        throw AssertionError("getFakeTag used not for SvelteBlock")
+    }
+
+    override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
+        if (isSvelteBlock(myNode)) {
+            return ChildAttributes(Indent.getNormalIndent(), null)
+        }
+
+        return super.getChildAttributes(newChildIndex)
+    }
+
+    override fun getChildrenIndent(): Indent {
+        if (isSvelteBlock(myNode)) {
+            return Indent.getNormalIndent()
+        }
+
+        return super.getChildrenIndent()
+    }
+    // end getTag related overrides
+
+    override fun buildChildren(): List<Block> {
+        if (isSvelteBlock(myNode)) {
+            return buildSvelteChildren()
+        }
+
+        return super.buildChildren()
+    }
+
+    abstract fun buildSvelteChildren(): List<Block>
+
+    override fun processSimpleChild(
+        child: ASTNode,
+        indent: Indent?,
+        result: MutableList<in Block>,
+        wrap: Wrap?,
+        alignment: Alignment?
+    ) {
+        if (isSvelteBlock(child)) {
+            result.add(createTagBlock(child, indent ?: Indent.getNoneIndent(), wrap, alignment))
+        } else {
+            super.processSimpleChild(child, indent, result, wrap, alignment)
+        }
+    }
+
+    @Suppress("UnstableApiUsage")
+    override fun createSimpleChild(child: ASTNode?, indent: Indent?, wrap: Wrap?, alignment: Alignment?): XmlBlock {
+        return createSimpleChild(myXmlFormattingPolicy, child, indent, wrap, alignment)
+    }
+
+    override fun createTagBlock(child: ASTNode?, indent: Indent?, wrap: Wrap?, alignment: Alignment?): XmlTagBlock {
+        return createTagBlock(myXmlFormattingPolicy, child, indent, wrap, alignment)
+    }
+}
+
+private fun AbstractXmlBlock.createSimpleChild(
+    xmlFormattingPolicy: XmlFormattingPolicy?,
+    child: ASTNode?,
+    indent: Indent?,
+    wrap: Wrap?,
+    alignment: Alignment?
+): XmlBlock {
+    return SvelteXmlBlock(child, wrap, alignment, xmlFormattingPolicy, indent, null, isPreserveSpace)
+}
+
+private fun AbstractXmlBlock.createTagBlock(
+    xmlFormattingPolicy: XmlFormattingPolicy?,
+    child: ASTNode?,
+    indent: Indent?,
+    wrap: Wrap?,
+    alignment: Alignment?
+): XmlTagBlock {
+    val newIndent = indent ?: Indent.getNoneIndent()
+    return SvelteXmlTagBlock(child, wrap, alignment, xmlFormattingPolicy, newIndent, isPreserveSpace)
+}
+
+private fun isSvelteBlock(child: ASTNode): Boolean {
+    return child.psi is SvelteBlock
+}
---
+++ src/main/java/dev/blachut/svelte/lang/format/SvelteXmlTagBlock.kt
@@ -0,0 +1,101 @@
+package dev.blachut.svelte.lang.format
+
+import com.intellij.formatting.*
+import com.intellij.lang.ASTNode
+import com.intellij.lang.javascript.JSTokenTypes
+import com.intellij.psi.formatter.xml.AbstractXmlBlock
+import com.intellij.psi.formatter.xml.XmlFormattingPolicy
+import com.intellij.psi.xml.XmlTag
+import dev.blachut.svelte.lang.psi.SvelteElementTypes
+import dev.blachut.svelte.lang.psi.SvelteEndTag
+import dev.blachut.svelte.lang.psi.SvelteTagElementTypes
+
+class SvelteXmlTagBlock(
+    node: ASTNode?,
+    wrap: Wrap?,
+    alignment: Alignment?,
+    policy: XmlFormattingPolicy?,
+    indent: Indent?,
+    preserveSpace: Boolean
+) : SvelteXmlTagBlockBase(node, wrap, alignment, policy, indent, preserveSpace) {
+    override fun canWrapTagEnd(tag: XmlTag): Boolean {
+        if (SvelteHtmlPolicy.wrappingTags.contains(tag.name)) return true
+
+        return super.canWrapTagEnd(tag)
+    }
+
+    override fun buildSvelteChildren(): List<Block> {
+        val textWrap = Wrap.createWrap(AbstractXmlBlock.getWrapType(myXmlFormattingPolicy.getTextWrap(tag)), true)
+        val results = ArrayList<Block>(3)
+
+        var child = myNode.firstChildNode
+        while (child != null) {
+            if (!AbstractXmlBlock.containsWhiteSpacesOnly(child) && child.textLength > 0) {
+                if (SvelteElementTypes.BRANCHES.contains(child.elementType)) {
+                    val tag = child.firstChildNode
+                    val fragment = child.lastChildNode
+
+                    processTag(results, tag)
+                    processFragment(results, fragment, textWrap)
+                } else if (child.psi is SvelteEndTag) {
+                    processTag(results, child)
+                }
+            }
+
+            child = child.treeNext
+        }
+
+        return results
+    }
+
+    private fun processTag(results: ArrayList<Block>, tag: ASTNode) {
+        val localResults = ArrayList<Block>(4)
+
+        var child = tag.firstChildNode
+        while (child != null) {
+            if (!AbstractXmlBlock.containsWhiteSpacesOnly(child) && child.textLength > 0) {
+                if (child.elementType === JSTokenTypes.LBRACE) {
+                    val startTag = SvelteTagElementTypes.START_TAGS.contains(tag.elementType)
+                    val wrap = if (startTag) Wrap.createWrap(WrapType.ALWAYS, true) else null
+                    localResults.add(createSimpleChild(child, null, wrap, null))
+                } else if (child.elementType === JSTokenTypes.RBRACE) {
+                    localResults.add(createSimpleChild(child, Indent.getNoneIndent(), null, null))
+                } else {
+                    child = processChild(localResults, child, null, null, null)
+                }
+            }
+
+            if (child != null) {
+                child = child.treeNext
+            }
+        }
+
+        // Same as XmlTagBlock.createTagDescriptionNode
+        results.add(createSyntheticBlock(localResults, null))
+    }
+
+    private fun processFragment(results: ArrayList<Block>, fragment: ASTNode, textWrap: Wrap?) {
+        val localResults = ArrayList<Block>(1)
+
+        var child = fragment.firstChildNode
+        while (child != null) {
+            if (!AbstractXmlBlock.containsWhiteSpacesOnly(child) && child.textLength > 0) {
+                val wrap = chooseWrap(child, null, null, textWrap)
+
+                child = processChild(localResults, child, wrap, null, Indent.getNormalIndent())
+            }
+
+            if (child != null) {
+                child = child.treeNext
+            }
+        }
+
+        if (localResults.isNotEmpty()) {
+            results.add(createSyntheticBlock(localResults, Indent.getNormalIndent()))
+        }
+    }
+
+    override fun createSyntheticBlock(localResult: ArrayList<Block>?, childrenIndent: Indent?): Block {
+        return SvelteSyntheticBlock(localResult, this, Indent.getNoneIndent(), myXmlFormattingPolicy, childrenIndent)
+    }
+}
