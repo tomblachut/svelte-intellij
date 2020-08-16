fff75fd14d14ae9a037d170234d7701769d78ef5
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-08-16 21:58:04 +0200

    Support Find Usages for subscribed references

---
+++ src/main/java/dev/blachut/svelte/lang/index/SvelteIdIndexer.kt
@@ -0,0 +1,25 @@
+package dev.blachut.svelte.lang.index
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import com.intellij.psi.impl.cache.impl.BaseFilterLexerUtil
+import com.intellij.psi.impl.cache.impl.id.IdIndexEntry
+import com.intellij.psi.impl.cache.impl.id.LexingIdIndexer
+import com.intellij.util.indexing.FileContent
+import dev.blachut.svelte.lang.SvelteHTMLLanguage
+
+class SvelteIdIndexer : LexingIdIndexer {
+    override fun map(inputData: FileContent): Map<IdIndexEntry, Int> {
+        return BaseFilterLexerUtil.scanContent(inputData) { consumer ->
+            SvelteFilterLexer(
+                consumer,
+                SyntaxHighlighterFactory.getSyntaxHighlighter(
+                    SvelteHTMLLanguage.INSTANCE, inputData.project, inputData.file
+                ).highlightingLexer
+            )
+        }.idMap
+    }
+
+    override fun getVersion(): Int {
+        return 1
+    }
+}
