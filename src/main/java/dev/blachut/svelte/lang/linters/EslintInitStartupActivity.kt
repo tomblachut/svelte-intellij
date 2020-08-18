e0f986e036fc684e519ceb1a10bcfcce7625207f
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2020-08-18 14:39:50 +0500

    enable eslint support for svelte files, fixes #24

---
+++ src/main/java/dev/blachut/svelte/lang/linters/EslintInitStartupActivity.kt
@@ -0,0 +1,28 @@
+package dev.blachut.svelte.lang.linters
+
+import com.intellij.ide.util.RunOnceUtil
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.util.registry.Registry
+import java.util.*
+
+class EslintInitStartupActivity : StartupActivity {
+
+    override fun runActivity(project: Project) {
+        val key = "eslint.additional.file.extensions"
+
+        RunOnceUtil.runOnceForApp("svelte.init.key") {
+            try {
+                val stringValue = Registry.stringValue(key)
+                if (!stringValue.contains("svelte")) {
+                    val newValue = if (stringValue.isEmpty()) "svelte" else "$stringValue,svelte"
+                    val value = Registry.get(key)
+                    value.setValue(newValue)
+                }
+            } catch (e: MissingResourceException) {
+                Logger.getInstance(EslintInitStartupActivity::class.java).warn("Cannot find the key: $key")
+            }
+        }
+    }
+}
