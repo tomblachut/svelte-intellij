13cc40af3ca8a4c06cafe142eb2790669f61c45a
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2020-09-01 12:38:58 +0300

    more generic test

--- src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteTypeScriptHighlightingTest.kt
+++ src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteTypeScriptHighlightingTest.kt
@@ -13,6 +13,14 @@ class SvelteTypeScriptHighlightingTest : BasePlatformTestCase() {
             """
                 <script lang="ts">
                     export let testName: string;
+
+                    declare let test:number;
+                    if (test) {
+                        test = <error>"hello"</error>;
+                    } else {
+                        test = 2;
+                    }
+                    console.log(test);
                 </script>
                 <title>{testName}</title>
                 """.trimIndent())
