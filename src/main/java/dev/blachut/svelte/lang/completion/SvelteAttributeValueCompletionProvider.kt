07607214f7dceb96b4570422cb408636b07aa3eb
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-10-18 01:12:16 +0200

    Add completions for script lang (#165)

--- src/main/java/dev/blachut/svelte/lang/completion/SvelteAttributeValueCompletionProvider.kt
+++ src/main/java/dev/blachut/svelte/lang/completion/SvelteAttributeValueCompletionProvider.kt
@@ -12,9 +12,14 @@ import com.intellij.util.ProcessingContext
 import java.util.*
 
 class SvelteAttributeValueCompletionProvider : CompletionProvider<CompletionParameters>() {
+    private val scriptLanguages = scriptLanguages()
     private val styleLanguages = styleLanguages()
 
-    override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
         val xmlTag = PsiTreeUtil.getParentOfType(parameters.position, XmlTag::class.java, false)
         val xmlAttribute = PsiTreeUtil.getParentOfType(parameters.position, XmlAttribute::class.java, false)
         if (xmlTag == null || xmlAttribute == null) return
@@ -25,11 +30,21 @@ class SvelteAttributeValueCompletionProvider : CompletionProvider<CompletionPara
     }
 
     private fun listOfCompletions(xmlTag: XmlTag, xmlAttribute: XmlAttribute): Set<String> {
-        return if (xmlTag.name == "style" && xmlAttribute.name == "lang") {
-            styleLanguages
-        } else {
-            emptySet()
+        if (xmlAttribute.name == "lang") {
+            when (xmlTag.name) {
+                "script" -> return scriptLanguages
+                "style" -> return styleLanguages
+            }
         }
+
+        return emptySet()
+    }
+
+    private fun scriptLanguages(): Set<String> {
+        val result = mutableListOf<String>()
+        result.add("js")
+        result.add("ts")
+        return result.toSet()
     }
 
     // Taken from Vue plugin
