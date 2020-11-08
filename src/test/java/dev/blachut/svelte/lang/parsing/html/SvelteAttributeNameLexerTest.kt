b9f0200a5c40e9f49861e2d64bf2d31542f79200
Tomasz Błachut <blachut.tomasz@gmail.com>
2020-11-08 18:23:53 +0100

    directives lexer

---
+++ src/test/java/dev/blachut/svelte/lang/parsing/html/SvelteAttributeNameLexerTest.kt
@@ -0,0 +1,50 @@
+package dev.blachut.svelte.lang.parsing.html
+
+import com.intellij.lexer.Lexer
+import com.intellij.testFramework.LexerTestCase
+
+class SvelteAttributeNameLexerTest : LexerTestCase() {
+    override fun getDirPath(): String = throw NotImplementedError()
+
+    override fun createLexer(): Lexer {
+        return SvelteAttributeNameLexer()
+    }
+
+    fun testOn() = doTest(
+        "on:click|once|capture", """
+        JS:IDENTIFIER ('on:')
+        JS:IDENTIFIER ('click')
+        JS:OR ('|')
+        XML_NAME ('once')
+        JS:OR ('|')
+        XML_NAME ('capture')
+    """.trimIndent()
+    )
+
+    fun testLet() = doTest(
+        "let:slottedVariable", """
+        JS:IDENTIFIER ('let:')
+        JS:IDENTIFIER ('slottedVariable')
+    """.trimIndent()
+    )
+
+    fun testUseDotted() = doTest(
+        "use:obj.action", """
+        JS:IDENTIFIER ('use:')
+        JS:IDENTIFIER ('obj')
+        JS:DOT ('.')
+        JS:IDENTIFIER ('action')
+    """.trimIndent()
+    )
+
+    fun testOutDottedWithModifiers() = doTest(
+        "out:obj.fade|local", """
+        JS:IDENTIFIER ('out:')
+        JS:IDENTIFIER ('obj')
+        JS:DOT ('.')
+        JS:IDENTIFIER ('fade')
+        JS:OR ('|')
+        XML_NAME ('local')
+    """.trimIndent()
+    )
+}
