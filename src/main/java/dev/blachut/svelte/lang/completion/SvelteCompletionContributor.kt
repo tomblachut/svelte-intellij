d90229fe2abfb454ef98befec2a944e122d0af2e
Tomasz Błachut <blachut.tomasz@gmail.com>
2021-01-25 10:04:50 +0100

    Add keyword completions (#198)

--- src/main/java/dev/blachut/svelte/lang/completion/SvelteCompletionContributor.kt
+++ src/main/java/dev/blachut/svelte/lang/completion/SvelteCompletionContributor.kt
@@ -2,17 +2,30 @@ package dev.blachut.svelte.lang.completion
 
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.lang.javascript.JSTokenTypes
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PlatformPatterns.psiFile
 import com.intellij.patterns.XmlPatterns
 import com.intellij.psi.xml.XmlTokenType
 import dev.blachut.svelte.lang.psi.SvelteHtmlFile
+import dev.blachut.svelte.lang.psi.SvelteJSLazyElementTypes
 
 class SvelteCompletionContributor : CompletionContributor() {
     init {
-        extend(CompletionType.BASIC, psiElement(XmlTokenType.XML_NAME).withParent(XmlPatterns.xmlAttribute()).inFile(psiFile(SvelteHtmlFile::class.java)),
-            SvelteAttributeNameCompletionProvider())
-        extend(CompletionType.BASIC, psiElement(XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN).inFile(psiFile(SvelteHtmlFile::class.java)),
-            SvelteAttributeValueCompletionProvider())
+        extend(
+            CompletionType.BASIC,
+            psiElement(XmlTokenType.XML_NAME).withParent(XmlPatterns.xmlAttribute()).inFile(psiFile(SvelteHtmlFile::class.java)),
+            SvelteAttributeNameCompletionProvider()
+        )
+        extend(
+            CompletionType.BASIC,
+            psiElement(XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN).inFile(psiFile(SvelteHtmlFile::class.java)),
+            SvelteAttributeValueCompletionProvider()
+        )
+        extend(
+            CompletionType.BASIC,
+            psiElement(JSTokenTypes.IDENTIFIER).withAncestor(2, psiElement(SvelteJSLazyElementTypes.CONTENT_EXPRESSION)).inFile(psiFile(SvelteHtmlFile::class.java)),
+            SvelteKeywordCompletionProvider()
+        )
     }
 }
--- src/main/java/dev/blachut/svelte/lang/editor/SvelteAutoPopupHandler.kt
+++ src/main/java/dev/blachut/svelte/lang/editor/SvelteAutoPopupHandler.kt
@@ -7,23 +7,51 @@ import com.intellij.codeInsight.lookup.LookupManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.elementType
+import com.intellij.psi.util.parentOfType
 import dev.blachut.svelte.lang.directives.SvelteDirectiveUtil
+import dev.blachut.svelte.lang.isScriptOrStyleTag
 import dev.blachut.svelte.lang.isSvelteContext
+import dev.blachut.svelte.lang.psi.SvelteHtmlElementTypes
 import dev.blachut.svelte.lang.psi.SvelteHtmlTag
+import dev.blachut.svelte.lang.psi.SvelteJSLazyElementTypes
 
 class SvelteAutoPopupHandler : TypedHandlerDelegate() {
+    private val attributeExpectedChars =
+        setOf(SvelteDirectiveUtil.DIRECTIVE_SEPARATOR, SvelteDirectiveUtil.MODIFIER_SEPARATOR)
+
     override fun checkAutoPopup(charTyped: Char, project: Project, editor: Editor, file: PsiFile): Result {
         if (LookupManager.getActiveLookup(editor) != null) return Result.CONTINUE
         if (!isSvelteContext(file)) return Result.CONTINUE
 
-        val element = file.findElementAt(editor.caretModel.offset)
-        if (element?.parent !is SvelteHtmlTag) return Result.CONTINUE
+        val offset = editor.caretModel.offset
+        val element = file.findElementAt(offset)
+            ?: file.findElementAt(offset - 1)
+            ?: return Result.CONTINUE
 
-        if (charTyped == SvelteDirectiveUtil.DIRECTIVE_SEPARATOR || charTyped == SvelteDirectiveUtil.MODIFIER_SEPARATOR) {
-            AutoPopupController.getInstance(project).scheduleAutoPopup(editor)
-            return Result.STOP
+        if (element.parent.elementType == SvelteHtmlElementTypes.SVELTE_HTML_TAG) {
+            if (attributeExpectedChars.contains(charTyped)) {
+                return handle(project, editor)
+            }
+        }
+
+        val parentTag = element.parentOfType<SvelteHtmlTag>()
+
+        if (charTyped == '{' && (parentTag == null || !parentTag.isScriptOrStyleTag())) {
+            return handle(project, editor)
+        }
+
+        if (element.parent.elementType == SvelteJSLazyElementTypes.CONTENT_EXPRESSION) {
+            if (charTyped == '#' || charTyped == '@') {
+                return handle(project, editor)
+            }
         }
 
         return Result.CONTINUE
+    }
+
+    private fun handle(project: Project, editor: Editor): Result {
+        AutoPopupController.getInstance(project).scheduleAutoPopup(editor)
+        return Result.STOP
     }
 }
