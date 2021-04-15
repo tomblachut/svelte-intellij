e3d9f5612007bcb11d3f1a3aeda920371344cfd2
Tomasz Błachut <blachut.tomasz@gmail.com>
2021-04-15 21:23:55 +0200

    Remove deprecated methods from SvelteJSLanguage (#147)

--- src/main/java/dev/blachut/svelte/lang/psi/SvelteJSLazyElementType.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteJSLazyElementType.kt
@@ -3,11 +3,11 @@ package dev.blachut.svelte.lang.psi
 import com.intellij.lang.ASTNode
 import com.intellij.lang.PsiBuilder
 import com.intellij.lang.PsiBuilderFactory
+import com.intellij.lang.javascript.JSLanguageUtil
 import com.intellij.lang.javascript.parsing.JavaScriptParser
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.ILazyParseableElementType
 import dev.blachut.svelte.lang.SvelteJSLanguage
-import dev.blachut.svelte.lang.parsing.js.SvelteJSParser
 
 // TODO Merge SvelteJSBlockLazyElementType & SvelteJSLazyElementType
 abstract class SvelteJSLazyElementType(debugName: String) : ILazyParseableElementType(debugName, SvelteJSLanguage.INSTANCE) {
@@ -22,7 +22,7 @@ abstract class SvelteJSLazyElementType(debugName: String) : ILazyParseableElemen
     override fun doParseContents(chameleon: ASTNode, psi: PsiElement): ASTNode {
         val project = psi.project
         val builder = PsiBuilderFactory.getInstance().createBuilder(project, chameleon, null, language, chameleon.chars)
-        val parser = SvelteJSParser(builder)
+        val parser = JSLanguageUtil.createJSParser(language, builder)
 
         val rootMarker = builder.mark()
 
