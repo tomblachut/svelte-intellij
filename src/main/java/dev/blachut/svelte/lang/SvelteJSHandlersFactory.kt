a8cb81e88989ad524552b42d795dc89d3f48f161
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2021-06-30 12:09:46 +0200

    Place imports correctly inside script tag

---
+++ src/main/java/dev/blachut/svelte/lang/SvelteJSHandlersFactory.kt
@@ -0,0 +1,27 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package dev.blachut.svelte.lang
+
+import com.intellij.lang.ecmascript6.ES6HandlersFactory
+import com.intellij.lang.javascript.modules.imports.JSAddImportExecutor
+import com.intellij.lang.typescript.TypeScriptHandlersFactory
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import dev.blachut.svelte.lang.codeInsight.SvelteAddImportExecutor
+
+class SvelteHtmlHandlersFactory : ES6HandlersFactory() {
+    override fun createAddImportExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
+        return SvelteAddImportExecutor(editor, place)
+    }
+}
+
+class SvelteJSHandlersFactory : ES6HandlersFactory() {
+    override fun createAddImportExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
+        return SvelteAddImportExecutor(editor, place)
+    }
+}
+
+class SvelteTSHandlersFactory : TypeScriptHandlersFactory() {
+    override fun createAddImportExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
+        return SvelteAddImportExecutor(editor, place)
+    }
+}
---
+++ src/main/java/dev/blachut/svelte/lang/codeInsight/SvelteAddImportExecutor.kt
@@ -0,0 +1,23 @@
+package dev.blachut.svelte.lang.codeInsight
+
+import com.intellij.lang.ecmascript6.actions.ES6AddImportExecutor
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import dev.blachut.svelte.lang.psi.SvelteHtmlFile
+import dev.blachut.svelte.lang.psi.findAncestorScript
+import dev.blachut.svelte.lang.psi.getJsEmbeddedContent
+
+class SvelteAddImportExecutor(editor: Editor?, place: PsiElement) : ES6AddImportExecutor(editor, place) {
+    override fun prepareScopeToAdd(place: PsiElement, fromExternalModule: Boolean): PsiElement? {
+        val parentScript = findAncestorScript(place)
+        if (parentScript != null) {
+            // inside module or instance script
+            return getJsEmbeddedContent(parentScript)
+        }
+
+        // inside Svelte expression
+        val containingFile = place.containingFile as? SvelteHtmlFile ?: return null
+
+        return prepareInstanceScriptContent(containingFile)
+    }
+}
