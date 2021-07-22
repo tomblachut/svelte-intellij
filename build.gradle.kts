54c27e1ff51073116054d6fa387e6f0d52fad0a4
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2021-07-22 15:36:31 +0200

    Update gradle-intellij-plugin to 1.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
     // https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.1.2"
+    id("org.jetbrains.intellij") version "1.1.4"
     // https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.1.2"
     // https://github.com/JetBrains/gradle-grammar-kit-plugin
