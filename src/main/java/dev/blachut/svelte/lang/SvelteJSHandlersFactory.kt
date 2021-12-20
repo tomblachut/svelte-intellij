f56a4f4c8622d419806d6f52ac08bc51ea16f727
Andrew Starovoyt <andrey.starovoyt@jetbrains.com>
2021-12-20 17:51:32 +0500

    svelte: re-implement support for auto-imports, compatibility with 2021.3.1 (#259)

--- src/main/java/dev/blachut/svelte/lang/SvelteJSHandlersFactory.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteJSHandlersFactory.kt
@@ -2,25 +2,34 @@ package dev.blachut.svelte.lang
 
 import com.intellij.lang.ecmascript6.ES6HandlersFactory
 import com.intellij.lang.javascript.modules.imports.JSAddImportExecutor
+import com.intellij.lang.javascript.modules.imports.JSImportExecutorFactory
 import com.intellij.lang.typescript.TypeScriptHandlersFactory
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
 import dev.blachut.svelte.lang.codeInsight.SvelteAddImportExecutor
 
+fun createSvelteImportFactories(): List<JSImportExecutorFactory> {
+    return listOf(object : JSImportExecutorFactory {
+        override fun createExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
+            return SvelteAddImportExecutor(editor, place)
+        }
+    })
+}
+
 class SvelteHtmlHandlersFactory : ES6HandlersFactory() {
-    override fun createAddImportExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
-        return SvelteAddImportExecutor(editor, place)
+    override fun createImportFactories(place: PsiElement): List<JSImportExecutorFactory> {
+        return createSvelteImportFactories()
     }
 }
 
 class SvelteJSHandlersFactory : ES6HandlersFactory() {
-    override fun createAddImportExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
-        return SvelteAddImportExecutor(editor, place)
+    override fun createImportFactories(place: PsiElement): List<JSImportExecutorFactory> {
+        return createSvelteImportFactories()
     }
 }
 
 class SvelteTSHandlersFactory : TypeScriptHandlersFactory() {
-    override fun createAddImportExecutor(editor: Editor?, place: PsiElement): JSAddImportExecutor {
-        return SvelteAddImportExecutor(editor, place)
+    override fun createImportFactories(place: PsiElement): List<JSImportExecutorFactory> {
+        return createSvelteImportFactories()
     }
 }
