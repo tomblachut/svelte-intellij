6daa7c1955431e0a9db21b9437e29ed5dfd079bf
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2022-07-19 12:14:50 +0200

    [svelte] add SvelteTestSuite
    GitOrigin-RevId: 78a37ec1f21a2c450a756e5b45e213e2b4d495cd

---
+++ src/test/java/dev/blachut/svelte/lang/SvelteTestSuite.kt
@@ -0,0 +1,40 @@
+package dev.blachut.svelte.lang
+
+import dev.blachut.svelte.lang.codeInsight.*
+import dev.blachut.svelte.lang.editor.SvelteBraceTypedTest
+import dev.blachut.svelte.lang.editor.SvelteCommenterTest
+import dev.blachut.svelte.lang.editor.SvelteEditorTest
+import dev.blachut.svelte.lang.editor.SvelteEmmetTest
+import dev.blachut.svelte.lang.format.SvelteFormatterTest
+import dev.blachut.svelte.lang.parsing.html.*
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    // Parsing
+    ExtendableHtmlParsingTest::class,
+    SvelteHighlightingLexerTest::class,
+    SvelteHtmlParserTest::class,
+    SvelteDirectiveLexerTest::class,
+    SvelteDirectiveParserTest::class,
+    // Editing
+    SvelteFormatterTest::class,
+    SvelteBraceTypedTest::class,
+    SvelteCommenterTest::class,
+    SvelteEditorTest::class,
+    SvelteEmmetTest::class,
+    // Code insight
+    SvelteAutoPopupTest::class,
+    SvelteCompletionTest::class,
+    SvelteHighlightingTest::class,
+    SvelteResolveTest::class,
+    SvelteNavigationTest::class,
+    SvelteTypeScriptCompletionTest::class,
+    SvelteTypeScriptHighlightingTest::class,
+    SvelteCreateVariableTest::class,
+    SvelteCreateFunctionTest::class,
+    SvelteCreateImportTest::class,
+    SvelteKitTest::class,
+)
+class SvelteTestSuite
