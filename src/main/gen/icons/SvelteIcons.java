2fdd5720b37016f8d291f1f21f049ba902e3a216
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-27 09:01:26 +0100

    use XXH3 instead of MurMur3 to generate an icon image key
    The main reason — use XXH3 everywhere
    
    GitOrigin-RevId: 7b158b9b3689d16f09182abb0b58113c7303f874

--- src/main/gen/icons/SvelteIcons.java
+++ src/main/gen/icons/SvelteIcons.java
@@ -13,8 +13,8 @@ public final class SvelteIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, SvelteIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Desaturated = load("icons/desaturated.svg", 205022573, 0);
-  /** 16x16 */ public static final @NotNull Icon Gray = load("icons/gray.svg", -1399028150, 0);
-  /** 16x16 */ public static final @NotNull Icon Original = load("icons/original.svg", -1417320023, 0);
-  /** 16x16 */ public static final @NotNull Icon Saturated = load("icons/saturated.svg", 93199846, 0);
+  /** 16x16 */ public static final @NotNull Icon Desaturated = load("icons/desaturated.svg", 1287590622, 0);
+  /** 16x16 */ public static final @NotNull Icon Gray = load("icons/gray.svg", -124814192, 0);
+  /** 16x16 */ public static final @NotNull Icon Original = load("icons/original.svg", 1084306452, 0);
+  /** 16x16 */ public static final @NotNull Icon Saturated = load("icons/saturated.svg", -760472411, 0);
 }
