33e13b2390540d9e337bc7ecd6484ece85532c4c
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-02-24 18:48:25 +0100

    WEB-56549 Refactor HtmlParsing API to make it more flexible and readable.
    GitOrigin-RevId: e2befe44ad88b9a47a497580f5c82c9b0af150eb

--- src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlParsing.kt
+++ src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlParsing.kt
@@ -1,6 +1,7 @@
 package dev.blachut.svelte.lang.parsing.html
 
 import com.intellij.lang.PsiBuilder
+import com.intellij.lang.PsiBuilder.Marker
 import com.intellij.lang.html.HtmlParsing
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.xml.XmlElementType
@@ -14,7 +15,6 @@ import dev.blachut.svelte.lang.isTokenAfterWhiteSpace
 import dev.blachut.svelte.lang.psi.*
 
 class SvelteHtmlParsing(builder: PsiBuilder) : HtmlParsing(builder) {
-    private val blockLevel get() = if (openedBlocks.empty()) 0 else openedBlocks.peek().tagLevel
 
     private val openedBlocks = Stack<OpenedBlock>()
 
@@ -22,49 +22,53 @@ class SvelteHtmlParsing(builder: PsiBuilder) : HtmlParsing(builder) {
         val (tagToken, tagMarker) = SvelteTagParsing.parseTag(builder)
 
         if (SvelteTagElementTypes.START_TAGS.contains(tagToken)) {
-            val blockTagLevel = tagLevel() + 1
+            val blockTagLevel = stackSize() + 1
             val openedBlock = SvelteBlockParsing.startBlock(blockTagLevel, tagToken, tagMarker, builder.mark())
-            pushTag(openedBlock.outerMarker, SYNTHETIC_TAG, SYNTHETIC_TAG)
+            pushItemToStack(SyntheticTag(tagMarker))
             openedBlocks.push(openedBlock)
-        } else if (SvelteTagElementTypes.INNER_TAGS.contains(tagToken)) {
+        }
+        else if (SvelteTagElementTypes.INNER_TAGS.contains(tagToken)) {
             if (!openedBlocks.empty() && openedBlocks.peek().isMatchingInnerTag(tagToken)) {
                 val openedBlock = openedBlocks.peek()
 
                 flushHtmlTags(tagMarker, openedBlock.tagLevel)
                 openedBlock.handleInnerTag(tagToken, tagMarker, builder.mark())
-            } else {
+            }
+            else {
                 tagMarker.precede().errorBefore(SvelteBundle.message("svelte.parsing.error.unexpected.inner.tag"), tagMarker)
             }
-        } else if (SvelteTagElementTypes.END_TAGS.contains(tagToken)) {
+        }
+        else if (SvelteTagElementTypes.END_TAGS.contains(tagToken)) {
             if (!openedBlocks.empty() && openedBlocks.peek().isMatchingEndTag(tagToken)) {
                 val openedBlock = openedBlocks.pop()
 
                 flushHtmlTags(tagMarker, openedBlock.tagLevel)
-                closeTag() // SYNTHETIC_TAG
+                popItemFromStack() // SYNTHETIC_TAG
                 openedBlock.handleEndTag(tagMarker)
-            } else {
+            }
+            else {
                 tagMarker.precede().errorBefore(SvelteBundle.message("svelte.parsing.error.unexpected.end.tag"), tagMarker)
             }
         }
     }
 
-    override fun getHtmlTagElementType(): IElementType {
+    override fun getHtmlTagElementType(info: HtmlTagInfo, tagLevel: Int): IElementType {
         return SvelteHtmlElementTypes.SVELTE_HTML_TAG
     }
 
-    override fun isSingleTag(tagName: String, originalTagName: String): Boolean {
+    override fun isSingleTag(tagInfo: HtmlTagInfo): Boolean {
         // Inspired by Vue plugin. Svelte tags must be closed explicitly
-        if (isSvelteComponentTag(originalTagName)) {
+        if (isSvelteComponentTag(tagInfo.originalName)) {
             return false
         }
-        return super.isSingleTag(tagName, originalTagName)
+        return super.isSingleTag(tagInfo)
     }
 
     override fun hasCustomTagContent(): Boolean {
         return token() === SvelteTokenTypes.START_MUSTACHE
     }
 
-    override fun parseCustomTagContent(xmlText: PsiBuilder.Marker?): PsiBuilder.Marker? {
+    override fun parseCustomTagContent(xmlText: Marker?): Marker? {
         terminateText(xmlText)
         parseSvelteTag()
         return null
@@ -74,7 +78,7 @@ class SvelteHtmlParsing(builder: PsiBuilder) : HtmlParsing(builder) {
         return hasCustomTagContent()
     }
 
-    override fun parseCustomTopLevelContent(error: PsiBuilder.Marker?): PsiBuilder.Marker? {
+    override fun parseCustomTopLevelContent(error: Marker?): Marker? {
         flushError(error)
         parseSvelteTag()
         return null
@@ -85,52 +89,21 @@ class SvelteHtmlParsing(builder: PsiBuilder) : HtmlParsing(builder) {
             val openedBlock = openedBlocks.pop()
             val marker = builder.mark()
             flushHtmlTags(marker, openedBlock.tagLevel)
-            closeTag() // SYNTHETIC_TAG
+            popItemFromStack() // SYNTHETIC_TAG
             openedBlock.handleMissingEndTag(marker)
         }
 
         super.flushOpenTags()
     }
 
-    override fun hasRealTags(): Boolean {
-        return hasTags() && peekTagName() != SYNTHETIC_TAG
-    }
-
-    override fun isTagNameFurtherInStack(endName: String, tagNames: Stack<String>): Boolean {
-        if (!tagNames.empty() && tagNames.peek() == SYNTHETIC_TAG) {
-            return false
-        }
-
-        return super.isTagNameFurtherInStack(endName, tagNames)
-    }
-
-    override fun canOpeningTagAutoClose(tagOnStack: String, openingTag: String, tagLevel: Int): Boolean? {
-        if (tagLevel <= blockLevel) return false
-
-        return super.canOpeningTagAutoClose(tagOnStack, openingTag, tagLevel)
-    }
-
-    override fun canClosingTagAutoClose(tagOnStack: String, closingTag: String): Boolean {
-        if (tagLevel() <= blockLevel) return false
-
-        return super.canClosingTagAutoClose(tagOnStack, closingTag)
-    }
-
-    override fun terminateAutoClosingParentTag(tag: PsiBuilder.Marker, tagName: String) {
-        if (tagLevel() <= blockLevel) return
-
-        super.terminateAutoClosingParentTag(tag, tagName)
-    }
-
-    private fun flushHtmlTags(beforeMarker: PsiBuilder.Marker, targetTagLevel: Int) {
-        while (tagLevel() > targetTagLevel) {
-            val tagName = peekTagName()
-            if (isEndTagRequired(tagName)) {
+    private fun flushHtmlTags(beforeMarker: Marker, targetTagLevel: Int) {
+        while (stackSize() > targetTagLevel) {
+            val tagInfo = peekTagInfo()
+            if (isEndTagRequired(tagInfo)) {
                 val errorMarker = beforeMarker.precede()
-                errorMarker.errorBefore(XmlPsiBundle.message("xml.parsing.named.element.is.not.closed", tagName), beforeMarker)
+                errorMarker.errorBefore(XmlPsiBundle.message("xml.parsing.named.element.is.not.closed", tagInfo.originalName), beforeMarker)
             }
-            val tag = closeTag()
-            tag.doneBefore(htmlTagElementType, beforeMarker)
+            doneTagBefore(beforeMarker)
         }
     }
 
@@ -177,26 +150,32 @@ class SvelteHtmlParsing(builder: PsiBuilder) : HtmlParsing(builder) {
                     val error = mark()
                     advance()
                     error.error(XmlPsiBundle.message("xml.parsing.unescaped.ampersand.or.nonterminated.character.entity.reference"))
-                } else if (tt === XmlTokenType.XML_ENTITY_REF_TOKEN) {
+                }
+                else if (tt === XmlTokenType.XML_ENTITY_REF_TOKEN) {
                     parseReference()
-                } else if (tt === SvelteTokenTypes.START_MUSTACHE) {
+                }
+                else if (tt === SvelteTokenTypes.START_MUSTACHE) {
                     parseAttributeExpression(elementType)
-                } else {
+                }
+                else {
                     advance()
                 }
             }
 
             if (token() === XmlTokenType.XML_ATTRIBUTE_VALUE_END_DELIMITER) {
                 advance()
-            } else {
+            }
+            else {
                 error(XmlPsiBundle.message("xml.parsing.unclosed.attribute.value"))
             }
-        } else {
+        }
+        else {
             // Unquoted attr value. Unlike unmodified IntelliJ HTML this isn't necessary single token
             while (token() === XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN || token() === SvelteTokenTypes.START_MUSTACHE) {
                 if (token() === SvelteTokenTypes.START_MUSTACHE) {
                     parseAttributeExpression(elementType)
-                } else {
+                }
+                else {
                     advance()
                 }
 
@@ -223,8 +202,10 @@ class SvelteHtmlParsing(builder: PsiBuilder) : HtmlParsing(builder) {
         expressionMarker.done(SvelteElementTypes.ATTRIBUTE_EXPRESSION)
     }
 
-    companion object {
-        // starts with ! so it is impossible to create such name in Svelte file
-        private const val SYNTHETIC_TAG = "!svelte-synthetic-tag"
+    // TODO Merge with OpenedBlock
+    private class SyntheticTag(private val startMarker: Marker) : HtmlParserStackItem {
+        override fun getStartMarker(): Marker =
+            startMarker
     }
+
 }
