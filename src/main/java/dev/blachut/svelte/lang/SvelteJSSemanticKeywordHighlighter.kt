4a5702c9971af51b569a9c4411c6b4f97951e2ab
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2023-02-27 10:53:48 +0100

    [js] fix keyword highlighting in injections (WEB-58895)
    (cherry picked from commit 496b55dd83e3fc5357fd946bbc92a3beacd7bf7e)
    
    IJ-CR-103697
    
    GitOrigin-RevId: 2eac51826a048357659ae515a0bf2bffc4f05094

--- src/main/java/dev/blachut/svelte/lang/SvelteJSSemanticKeywordHighlighter.kt
+++ src/main/java/dev/blachut/svelte/lang/SvelteJSSemanticKeywordHighlighter.kt
@@ -1,47 +1,26 @@
 package dev.blachut.svelte.lang
 
-import com.intellij.codeInsight.daemon.impl.HighlightInfo
-import com.intellij.codeInsight.daemon.impl.UpdateHighlightersUtil
+import com.intellij.codeInsight.daemon.impl.HighlightVisitor
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder
 import com.intellij.lang.javascript.JSAnalysisHandlersFactory
+import com.intellij.lang.javascript.validation.JSKeywordHighlighterVisitor
 import com.intellij.lang.javascript.validation.JSSemanticKeywordHighlighter
-import com.intellij.openapi.editor.Document
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import dev.blachut.svelte.lang.psi.SvelteHtmlFile
-import java.util.*
 
-class SvelteJSSemanticKeywordHighlighter(private val myFile: PsiFile, document: Document) : JSSemanticKeywordHighlighter(myFile, document), DumbAware {
-    private val myHolder: HighlightInfoHolder = HighlightInfoHolder(myFile)
+class SvelteJSSemanticKeywordHighlighter : JSSemanticKeywordHighlighter() {
 
-    override fun doCollectInformation(progress: ProgressIndicator) {
-        myHolder.clear()
-        if (myFile is SvelteHtmlFile) {
-            val factory = JSAnalysisHandlersFactory.forLanguage(SvelteJSLanguage.INSTANCE)
-            myFile.acceptChildren(factory.createKeywordHighlighterVisitor(myHolder, SvelteJSLanguage.INSTANCE.optionHolder))
-        }
+    override fun suitableForFile(file: PsiFile): Boolean {
+        return file is SvelteHtmlFile
     }
 
-    override fun doApplyInformationToEditor() {
-        val highlights: MutableList<HighlightInfo?> = ArrayList()
+    override fun createKeywordHighlightingVisitor(owner: PsiElement, holder: HighlightInfoHolder): JSKeywordHighlighterVisitor {
+        val factory = JSAnalysisHandlersFactory.forLanguage(SvelteJSLanguage.INSTANCE)
+        return factory.createKeywordHighlighterVisitor(holder, SvelteJSLanguage.INSTANCE.optionHolder)
+    }
 
-        for (i in 0 until myHolder.size()) {
-            highlights.add(myHolder[i])
-        }
-
-        // TODO After dropping 2020.1: unwrap conditional
-        @Suppress("SENSELESS_COMPARISON")
-        if (myDocument != null) {
-            UpdateHighlightersUtil.setHighlightersToEditor(
-                myProject,
-                myDocument,
-                0,
-                myFile.textLength,
-                highlights,
-                colorsScheme,
-                id
-            )
-        }
+    override fun clone(): HighlightVisitor {
+        return SvelteJSSemanticKeywordHighlighter()
     }
 }
