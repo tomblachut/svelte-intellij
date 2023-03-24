fa6289e86fe4cfe908a6347787716e5ff05d9ca1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-03-24 15:08:47 +0100

    use komihash for icon classes to align rasterized key with a runtime cache key (same algo)
    GitOrigin-RevId: fec04acb91d4a69d5c50571d3408c9b0ce3fddd4

--- src/main/gen/icons/SvelteIcons.java
+++ src/main/gen/icons/SvelteIcons.java
@@ -13,8 +13,8 @@ public final class SvelteIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, SvelteIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Desaturated = load("icons/desaturated.svg", 1287590622, 0);
-  /** 16x16 */ public static final @NotNull Icon Gray = load("icons/gray.svg", -124814192, 0);
-  /** 16x16 */ public static final @NotNull Icon Original = load("icons/original.svg", 1084306452, 0);
-  /** 16x16 */ public static final @NotNull Icon Saturated = load("icons/saturated.svg", -760472411, 0);
+  /** 16x16 */ public static final @NotNull Icon Desaturated = load("icons/desaturated.svg", -1828046015, 0);
+  /** 16x16 */ public static final @NotNull Icon Gray = load("icons/gray.svg", 1950784302, 0);
+  /** 16x16 */ public static final @NotNull Icon Original = load("icons/original.svg", -938751982, 0);
+  /** 16x16 */ public static final @NotNull Icon Saturated = load("icons/saturated.svg", -1802859978, 0);
 }
