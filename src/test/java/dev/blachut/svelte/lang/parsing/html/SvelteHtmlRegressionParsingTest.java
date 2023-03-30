0dce8b24ef55a6b220f97abeac3cb7bd5b3dfc9e
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2023-03-30 12:01:24 +0200

    [xml]: Fix XmlParsingTest and extending classes, use Assume, separate lexer performance tests into separate class
    GitOrigin-RevId: 1e9334629181207880f29f94c9665b8ef078fe9c

--- src/test/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlRegressionParsingTest.java
+++ src/test/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlRegressionParsingTest.java
@@ -5,12 +5,14 @@ import com.intellij.html.HtmlParsingTest;
 import com.intellij.lang.LanguageASTFactory;
 import com.intellij.lexer.EmbeddedTokenTypesProvider;
 import dev.blachut.svelte.lang.SvelteHTMLLanguage;
+import dev.blachut.svelte.lang.parsing.js.SvelteJSParserDefinition;
+import org.junit.AssumptionViolatedException;
 
 import static dev.blachut.svelte.lang.SvelteTestUtilKt.getSvelteTestDataPath;
 
 public class SvelteHtmlRegressionParsingTest extends HtmlParsingTest {
     public SvelteHtmlRegressionParsingTest() {
-        super("dev/blachut/svelte/lang/parsing/html/regressionHtml", "svelte", new SvelteHTMLParserDefinition());
+        super("dev/blachut/svelte/lang/parsing/html/regressionHtml", "svelte", new SvelteHTMLParserDefinition(), new SvelteJSParserDefinition());
     }
 
     @Override
@@ -27,11 +29,11 @@ public class SvelteHtmlRegressionParsingTest extends HtmlParsingTest {
 
     @Override
     public void testSpecialTagsParsing() {
-        // Ignore
+        throw new AssumptionViolatedException("Ignore");
     }
 
     @Override
     public void testScriptWithinScript() {
-        // Ignore
+        throw new AssumptionViolatedException("Ignore");
     }
 }
