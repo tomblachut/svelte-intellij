55a393e4ae56226aa0b4110ae7d63a30ac9d379a
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-04-06 17:47:44 +0200

    [svelte] suppress comma expression inspection inside debug tag (WEB-60066)
    GitOrigin-RevId: b57b4789bcfab82b8d260c4d92f57086ec9ce45a

--- src/main/java/dev/blachut/svelte/lang/compatibility/SvelteInspectionSuppressor.kt
+++ src/main/java/dev/blachut/svelte/lang/compatibility/SvelteInspectionSuppressor.kt
@@ -3,17 +3,20 @@ package dev.blachut.svelte.lang.compatibility
 import com.intellij.codeInspection.InspectionSuppressor
 import com.intellij.codeInspection.SuppressQuickFix
 import com.intellij.lang.javascript.inspections.JSConstantReassignmentInspection
+import com.intellij.lang.javascript.psi.JSEmbeddedContent
+import com.intellij.lang.javascript.psi.JSStatement
 import com.intellij.lang.javascript.psi.ecma6.impl.JSXXmlLiteralExpressionImpl
 import com.intellij.lang.typescript.inspection.TypeScriptMissingConfigOptionInspection
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parents
 import com.sixrr.inspectjs.assignment.SillyAssignmentJSInspection
+import com.sixrr.inspectjs.confusing.CommaExpressionJSInspection
 import com.sixrr.inspectjs.confusing.PointlessBooleanExpressionJSInspection
 import com.sixrr.inspectjs.control.UnnecessaryLabelJSInspection
 import com.sixrr.inspectjs.validity.BadExpressionStatementJSInspection
 import dev.blachut.svelte.lang.codeInsight.SvelteReactiveDeclarationsUtil
 import dev.blachut.svelte.lang.equalsName
-import dev.blachut.svelte.lang.psi.SvelteHtmlFile
-import dev.blachut.svelte.lang.psi.SvelteJSReferenceExpression
+import dev.blachut.svelte.lang.psi.*
 
 class SvelteInspectionSuppressor : InspectionSuppressor {
     override fun isSuppressedFor(element: PsiElement, inspectionId: String): Boolean {
@@ -29,6 +32,14 @@ class SvelteInspectionSuppressor : InspectionSuppressor {
             val parent = element.parent
             if (parent is SvelteJSReferenceExpression && parent.isSubscribedReference) {
                 // TODO check if store is writable
+                return true
+            }
+        }
+        if (inspectionId.equalsName<CommaExpressionJSInspection>()) {
+            val parent = element.parents(false).first { it is JSEmbeddedContent || it is JSStatement }
+            if (parent is JSEmbeddedContent &&
+                parent.elementType == SvelteJSLazyElementTypes.CONTENT_EXPRESSION &&
+                parent.children.any { it.node.elementType == SvelteTokenTypes.DEBUG_KEYWORD }) {
                 return true
             }
         }
