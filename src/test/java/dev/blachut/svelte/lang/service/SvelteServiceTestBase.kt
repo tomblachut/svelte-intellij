3528797f2a0f2f96ded323011e9c1492e503e520
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-06-15 13:02:08 +0200

    [ts] extract BaseLspTypeScriptServiceTest (WEB-61379)
    GitOrigin-RevId: e33b61c39f21d20f1e0ff79511d834067bf62e13

--- src/test/java/dev/blachut/svelte/lang/service/SvelteServiceTestBase.kt
+++ src/test/java/dev/blachut/svelte/lang/service/SvelteServiceTestBase.kt
@@ -1,18 +1,14 @@
 package dev.blachut.svelte.lang.service
 
+import com.intellij.javascript.debugger.com.intellij.lang.javascript.service.BaseLspTypeScriptServiceTest
 import com.intellij.lang.javascript.library.typings.TypeScriptExternalDefinitionsRegistry
-import com.intellij.lang.javascript.modules.JSTempDirWithNodeInterpreterTest
-import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptLanguageServiceUtil
 import com.intellij.lang.typescript.library.download.TypeScriptDefinitionFilesDirectory
 import com.intellij.openapi.util.Disposer
-import com.intellij.testFramework.UsefulTestCase
-import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl
 import dev.blachut.svelte.lang.service.settings.SvelteServiceMode
 import dev.blachut.svelte.lang.service.settings.getSvelteServiceSettings
-import junit.framework.TestCase
 
-open class SvelteServiceTestBase : JSTempDirWithNodeInterpreterTest() {
+abstract class SvelteServiceTestBase : BaseLspTypeScriptServiceTest() {
   // compilerOptions taken from SvelteKit app
   protected val tsconfig = """
     {
@@ -43,26 +39,13 @@ open class SvelteServiceTestBase : JSTempDirWithNodeInterpreterTest() {
       serviceSettings.serviceMode = old
       TypeScriptLanguageServiceUtil.setUseService(false)
     }
-
     serviceSettings.serviceMode = SvelteServiceMode.ENABLED
-    (myFixture as CodeInsightTestFixtureImpl).canChangeDocumentDuringHighlighting(true)
 
     SvelteLspExecutableDownloader.getExecutableOrRefresh(project) // could run blocking download
     assertNotNull(SvelteLspExecutableDownloader.getExecutable())
   }
 
   protected fun assertCorrectService() {
-    val service = TypeScriptService.getForFile(project, file.virtualFile)
-    UsefulTestCase.assertInstanceOf(service, SvelteLspTypeScriptService::class.java)
-    TestCase.assertTrue(service!!.isServiceCreated())
-  }
-
-  protected fun doDefaultHighlightingTest(directory: Boolean) {
-    if (directory) {
-      doTestWithCopyDirectory()
-    }
-    else {
-      defaultTest()
-    }
+    assertCorrectServiceImpl<SvelteLspTypeScriptService>()
   }
 }
