adbd13755ac768d1f834f2b4121397e1b2576065
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-06-19 20:31:49 +0200

    [svelte] add SvelteKit case to SvelteServiceDocumentationTest (WEB-61565)
    GitOrigin-RevId: 9fc79c1fd18dbc62fd8c01c5e3138164c5b719ec

---
+++ src/test/resources/dev/blachut/svelte/lang/service/documentation/KitDataProp/src/app.d.ts
@@ -0,0 +1,16 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
+}
+
+export {};
+
+export interface Unrelated {
+  somePost: number;
+}
---
+++ src/test/resources/dev/blachut/svelte/lang/service/documentation/KitDataProp/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{JSON.stringify(data.<caret>somePost)}</p>
