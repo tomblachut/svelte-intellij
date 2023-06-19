a75f7ca418b1efb46e79045699674ea821a2e42c
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-06-19 20:31:49 +0200

    [svelte] add SvelteKit case to SvelteServiceDocumentationTest (WEB-61565)
    (cherry picked from commit 9fc79c1fd18dbc62fd8c01c5e3138164c5b719ec)
    
    IJ-CR-109002
    
    GitOrigin-RevId: f10c36e7cdbd88997f0539956fea0daeb02b9e90

--- src/test/java/dev/blachut/svelte/lang/service/SvelteServiceDocumentationTest.kt
+++ src/test/java/dev/blachut/svelte/lang/service/SvelteServiceDocumentationTest.kt
@@ -4,6 +4,7 @@ import com.intellij.lang.javascript.JSAbstractDocumentationTest
 import com.intellij.openapi.util.registry.RegistryManager
 import com.intellij.platform.lsp.tests.checkLspHighlighting
 import dev.blachut.svelte.lang.getRelativeSvelteTestDataPath
+import dev.blachut.svelte.lang.getSvelteTestDataPath
 import org.junit.Test
 
 /**
@@ -38,9 +39,22 @@ class SvelteServiceDocumentationTest : SvelteServiceTestBase() {
   @Test
   fun testNoDummyResolve2() = defaultComboTest()
 
-  private fun defaultComboTest() {
-    myFixture.configureByText("tsconfig.json", tsconfig)
-    myFixture.configureByFile(getTestName(false) + "." + extension)
+  @Test
+  fun testKitDataProp() = defaultComboTest(true)
+
+  private fun defaultComboTest(directory: Boolean = false) {
+    if (directory) {
+      copyDirectory()
+      myFixture.testDataPath = getSvelteTestDataPath()
+      myFixture.copyDirectoryToProject("dev/blachut/svelte/lang/_npm", "node_modules")
+      myFixture.testDataPath = testDataPath
+      myFixture.configureFromTempProjectFile("src/routes/+page.svelte")
+    }
+    else {
+      myFixture.configureByText("tsconfig.json", tsconfig)
+      myFixture.configureByFile(getTestName(false) + "." + extension)
+    }
+
     myFixture.checkLspHighlighting()
     assertCorrectService()
 
---
+++ src/test/resources/dev/blachut/svelte/lang/service/documentation/KitDataProp/.svelte-kit/generated/client/nodes/0.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@1.20.4_svelte@3.59.1+vite@4.3.9/node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
