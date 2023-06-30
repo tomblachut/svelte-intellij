2441d4403eb7962903fe1f9ed8137aaaf6b956c0
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-06-30 14:19:44 +0200

    [svelte] decouple SvelteServiceDocumentationTest from MDN documentation
    GitOrigin-RevId: 59fb4132d048fad51fdf3fbf7db5b1c230a6f4ef

--- src/test/resources/dev/blachut/svelte/lang/service/documentation/QualifiedReferenceToMethod.svelte
+++ src/test/resources/dev/blachut/svelte/lang/service/documentation/QualifiedReferenceToMethod.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-  let local = 323;
+  /**
+   * Yes, umber is a word and it's a color!
+   */
+  class Umber {
+    /**
+     * Returns a string representing an umber in CSS hex notation.
+     * @param _digits Number of digits after the hash. Must be in the range 6-6, inclusive.
+     */
+    toHex(_digits?: number): string {
+      return "#635147";
+    }
+  }
+
+  let local = new Umber();
 </script>
 
-{local.<caret>toFixed()}
+{local.<caret>toHex()}
