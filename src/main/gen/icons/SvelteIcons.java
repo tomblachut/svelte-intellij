ad027edf637ccbd7ec0e719141f3a418496bbc43
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-08-09 11:07:29 +0300

    IDEA-326151 update hash algo for icons
    GitOrigin-RevId: bb8f3acd0314190362619afb86151270b893f082

--- src/main/gen/icons/SvelteIcons.java
+++ src/main/gen/icons/SvelteIcons.java
@@ -13,8 +13,8 @@ public final class SvelteIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, SvelteIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Desaturated = load("icons/desaturated.svg", -1828046015, 0);
-  /** 16x16 */ public static final @NotNull Icon Gray = load("icons/gray.svg", 1950784302, 0);
-  /** 16x16 */ public static final @NotNull Icon Original = load("icons/original.svg", -938751982, 0);
-  /** 16x16 */ public static final @NotNull Icon Saturated = load("icons/saturated.svg", -1802859978, 0);
+  /** 16x16 */ public static final @NotNull Icon Desaturated = load("icons/desaturated.svg", -1065869149, 0);
+  /** 16x16 */ public static final @NotNull Icon Gray = load("icons/gray.svg", 1263601735, 0);
+  /** 16x16 */ public static final @NotNull Icon Original = load("icons/original.svg", 1033490455, 0);
+  /** 16x16 */ public static final @NotNull Icon Saturated = load("icons/saturated.svg", 505751707, 0);
 }
