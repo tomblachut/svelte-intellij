2f8b8194a3a990e3e72bcf04208bb3794d15aa9d
Andrey Starovoyt <Andrey.Starovoyt@jetbrains.com>
2023-08-17 17:56:00 +0300

    typescript: add collector for service WEB-62273
    (cherry picked from commit 78cb7173c79d93ff6669442924a13bfdecae9362)
    
    GitOrigin-RevId: cc7ab0f57a55e72415dbb26669abc8b89a057928

--- src/main/java/dev/blachut/svelte/lang/service/SvelteLspTypeScriptService.kt
+++ src/main/java/dev/blachut/svelte/lang/service/SvelteLspTypeScriptService.kt
@@ -21,6 +21,7 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.util.text.SemVer
 import dev.blachut.svelte.lang.psi.SvelteHtmlAttribute
 import org.eclipse.lsp4j.MarkupContent
 
@@ -31,8 +32,10 @@ import org.eclipse.lsp4j.MarkupContent
 class SvelteLspTypeScriptService(project: Project) : BaseLspTypeScriptService(project, SvelteLspServerSupportProvider::class.java) {
   override val name = "Svelte LSP"
   override val prefix = "Svelte"
-  override val serverVersion
-    get() = SvelteLspExecutableDownloader.calculateVersion(project)
+
+  override fun getServiceVersion(): SemVer {
+    return SvelteLspExecutableDownloader.calculateVersion(project)
+  }
 
   override fun createQuickInfoResponse(markupContent: MarkupContent): TypeScriptQuickInfoResponse {
     return TypeScriptQuickInfoResponse().apply {
@@ -93,4 +96,6 @@ class SvelteLspTypeScriptService(project: Project) : BaseLspTypeScriptService(pr
   }
 
   override fun isAcceptable(file: VirtualFile) = isServiceEnabledAndAvailable(project, file)
+
+  override fun getServiceId(): String = "svelte"
 }
