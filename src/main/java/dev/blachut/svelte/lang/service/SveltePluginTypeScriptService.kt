842f4e51dccf49294716497f8497251b4e40c0d5
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-10-20 13:38:49 +0200

    [svelte] typescript-svelte-plugin - send configurePlugin command (WEB-61610)
    GitOrigin-RevId: d3e7d454b49d16114cda3851390a971c91962aa1

--- src/main/java/dev/blachut/svelte/lang/service/SveltePluginTypeScriptService.kt
+++ src/main/java/dev/blachut/svelte/lang/service/SveltePluginTypeScriptService.kt
@@ -1,14 +1,36 @@
 package dev.blachut.svelte.lang.service
 
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceObject
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceSimpleCommand
 import com.intellij.lang.typescript.compiler.languageService.frameworks.DownloadableTypeScriptServicePlugin
 import com.intellij.lang.typescript.compiler.languageService.frameworks.PluggableTypeScriptService
+import com.intellij.lang.typescript.compiler.languageService.protocol.commands.*
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import java.util.function.Consumer
 
 private val plugin = DownloadableTypeScriptServicePlugin("Svelte", SvelteTypeScriptPluginPackageDownloader)
 
 class SveltePluginTypeScriptService(project: Project) : PluggableTypeScriptService(project, plugin) {
   override fun hasDependenciesReady(context: VirtualFile): Boolean {
     return isTypeScriptPluginEnabledAndAvailable(project, context)
+  }
+
+  override fun getInitialOpenCommands(): MutableMap<JSLanguageServiceSimpleCommand, Consumer<JSLanguageServiceObject>> {
+    val initialCommands = super.getInitialOpenCommands()
+    val result: MutableMap<JSLanguageServiceSimpleCommand, Consumer<JSLanguageServiceObject>> = linkedMapOf()
+    addConfigureCommand(result)
+
+    result.putAll(initialCommands)
+    return result
+  }
+
+  private fun addConfigureCommand(result: MutableMap<JSLanguageServiceSimpleCommand, Consumer<JSLanguageServiceObject>>) {
+    // https://github.com/sveltejs/language-tools/pull/2185
+    val arguments = ConfigurePluginRequestArguments(pluginName = "typescript-svelte-plugin",
+                                                    configuration = mapOf("name" to "typescript-svelte-plugin",
+                                                                          "enabled" to true,
+                                                                          "assumeIsSvelteProject" to true))
+    result[ConfigurePluginRequest(arguments)] = Consumer {}
   }
 }
