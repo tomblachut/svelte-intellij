3ed267120c29e610a6370680a412fc8679a7e995
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-10-22 18:31:39 +0200

    [ts] use TypeScriptService for resolve on qualifiers with implicit types (WEB-62260)
    GitOrigin-RevId: fb9a8430c5a1f986a64e541a3d0bae8fd84dce68

--- src/main/java/dev/blachut/svelte/lang/editor/SvelteGotoDeclarationHandler.kt
+++ src/main/java/dev/blachut/svelte/lang/editor/SvelteGotoDeclarationHandler.kt
@@ -1,6 +1,9 @@
 package dev.blachut.svelte.lang.editor
 
 import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler
+import com.intellij.lang.javascript.documentation.JSQuickNavigateBuilder
+import com.intellij.lang.javascript.psi.JSElement
+import com.intellij.lang.typescript.TypeScriptGotoDeclarationHandler
 import com.intellij.lang.typescript.getResultsFromService
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.Editor
@@ -8,13 +11,17 @@ import com.intellij.psi.PsiElement
 import dev.blachut.svelte.lang.isSvelteContext
 
 /**
- * Meant to supersede ALL GotoDeclarationHandlers for Svelte files, since TypeScriptGotoDeclarationHandler is executed too late.
+ * Meant to supersede ALL GotoDeclarationHandlers for Svelte files, since [TypeScriptGotoDeclarationHandler] is executed too late.
  */
 class SvelteGotoDeclarationHandler : GotoDeclarationHandler {
   override fun getGotoDeclarationTargets(sourceElement: PsiElement?, offset: Int, editor: Editor): Array<PsiElement>? {
     ApplicationManager.getApplication().assertReadAccessAllowed()
     sourceElement ?: return null
     if (!isSvelteContext(sourceElement)) return null
+    if (JSQuickNavigateBuilder.getOriginalElementOrParentIfLeaf(sourceElement) is JSElement) {
+      // pass to normal resolve and then to TypeScriptGotoDeclarationHandler
+      return null
+    }
     val project = editor.project ?: return null
     return getResultsFromService(project, sourceElement, editor)
   }
--- src/main/java/dev/blachut/svelte/lang/service/settings/SvelteServiceResolveUtil.kt
+++ src/main/java/dev/blachut/svelte/lang/service/settings/SvelteServiceResolveUtil.kt
@@ -1,12 +1,8 @@
 package dev.blachut.svelte.lang.service.settings
 
 import com.intellij.lang.javascript.psi.JSPsiReferenceElement
-import com.intellij.lang.typescript.compiler.TypeScriptCompilerService
-import com.intellij.lang.typescript.compiler.TypeScriptService
+import com.intellij.lang.typescript.compiler.getServiceNavigationTargets
 import com.intellij.openapi.util.registry.Registry
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiElement
-import com.intellij.util.ObjectUtils
 
 enum class SvelteServiceReferenceResolveMode {
   OFF,
@@ -24,15 +20,4 @@ fun tryRecheckResolveResults(expression: JSPsiReferenceElement): Boolean {
 
   if (expression.multiResolve(false).isNotEmpty()) return true
   return getServiceNavigationTargets(expression).isNullOrEmpty().not()
-}
-
-private fun getServiceNavigationTargets(expression: JSPsiReferenceElement): Array<PsiElement>? {
-  val service = TypeScriptService.getForFile(expression.project, expression.containingFile.virtualFile)
-  val document = PsiDocumentManager.getInstance(expression.project).getDocument(expression.containingFile)
-  if (document == null || service == null) return null
-  var identifier = TypeScriptCompilerService.adjustIntoIdentifier(expression)
-  if (identifier is JSPsiReferenceElement) { // todo move to adjustIntoIdentifier, and/or handle that inside getNavigationFor
-    identifier = ObjectUtils.coalesce(identifier.referenceNameElement, identifier)
-  }
-  return service.getNavigationFor(document, identifier)
 }
