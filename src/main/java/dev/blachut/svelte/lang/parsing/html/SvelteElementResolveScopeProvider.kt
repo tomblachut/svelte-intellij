6544bee704934fb79c9df15609af05fffc71de44
Aleksei Berezkin <aleksei.berezkin@jetbrains.com>
2023-11-16 11:25:35 +0100

    WEB-59503 Fixed resolve scope in Astro
    GitOrigin-RevId: e08e0272faa5cb8529a4beee2cb7fc3297a25f07

--- src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteElementResolveScopeProvider.kt
+++ src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteElementResolveScopeProvider.kt
@@ -1,28 +1,15 @@
 package dev.blachut.svelte.lang.parsing.html
 
 import com.intellij.lang.javascript.DialectDetector
-import com.intellij.lang.javascript.ecmascript6.TypeScriptResolveScopeProvider
+import com.intellij.lang.javascript.ecmascript6.TypeScriptWithFileTypeUnionInnerProvider
 import com.intellij.lang.javascript.psi.resolve.JSElementResolveScopeProvider
 import com.intellij.lang.javascript.psi.util.JSUtils
-import com.intellij.lang.typescript.library.TypeScriptLibraryProvider
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.xml.XmlFile
 import dev.blachut.svelte.lang.SvelteHtmlFileType
 
 class SvelteElementResolveScopeProvider : JSElementResolveScopeProvider {
-  private val tsProvider = object : TypeScriptResolveScopeProvider() {
-    override fun isApplicable(file: VirtualFile) = true
-
-    override fun restrictByFileType(file: VirtualFile,
-                                    libraryService: TypeScriptLibraryProvider,
-                                    moduleAndLibraryScope: GlobalSearchScope): GlobalSearchScope {
-      return super.restrictByFileType(file, libraryService, moduleAndLibraryScope)
-        .uniteWith(GlobalSearchScope.getScopeRestrictedByFileTypes(moduleAndLibraryScope, file.fileType))
-    }
-  }
-
   override fun getElementResolveScope(element: PsiElement): GlobalSearchScope? {
     // Refer to https://github.com/tomblachut/svelte-intellij/issues/170
     if (!element.isValid) return null
@@ -34,7 +21,7 @@ class SvelteElementResolveScopeProvider : JSElementResolveScopeProvider {
     val scriptTagContent = JSUtils.findScriptTagContent(psiFile)
 
     if (scriptTagContent != null && DialectDetector.isTypeScript(scriptTagContent)) {
-      return tsProvider.getResolveScope(psiFile.viewProvider.virtualFile, element.project)
+      return TypeScriptWithFileTypeUnionInnerProvider.getResolveScope(psiFile.viewProvider.virtualFile, element.project)
     }
     return null
   }
