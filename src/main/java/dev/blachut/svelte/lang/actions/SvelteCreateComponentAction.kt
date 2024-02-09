4115acf10e697dd45f90d7e0cabe54a8b10d4e83
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-02-09 10:26:53 +0100

    WEB-60729 Svelte template file shouldn't be suggested in a non-Svelte project
    GitOrigin-RevId: 55aa296f1e2c3c129a92dd272d100deb7c4fbb4a

--- src/main/java/dev/blachut/svelte/lang/actions/SvelteCreateComponentAction.kt
+++ src/main/java/dev/blachut/svelte/lang/actions/SvelteCreateComponentAction.kt
@@ -4,18 +4,29 @@ package dev.blachut.svelte.lang.actions
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
 import com.intellij.ide.fileTemplates.impl.CustomFileTemplate
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiFile
 import dev.blachut.svelte.lang.SvelteBundle
+import dev.blachut.svelte.lang.hasSvelteFiles
+import dev.blachut.svelte.lang.isSvelteContext
+import dev.blachut.svelte.lang.isSvelteProjectContext
 import icons.SvelteIcons
 
 class SvelteCreateComponentAction : CreateFileFromTemplateAction(), DumbAware {
   companion object {
     private const val TEMPLATE_NAME: String = "Svelte Component"
   }
+
+  override fun isAvailable(dataContext: DataContext): Boolean =
+    super.isAvailable(dataContext)
+    && (PlatformDataKeys.PROJECT.getData(dataContext)?.let { hasSvelteFiles(it) } == true
+        || (PlatformDataKeys.PSI_ELEMENT.getData(dataContext)
+            ?: PlatformDataKeys.PSI_FILE.getData(dataContext))?.let { isSvelteProjectContext(it) } == true)
 
   override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
     val name = SvelteBundle.message("svelte.component")
--- src/main/java/dev/blachut/svelte/lang/utils.kt
+++ src/main/java/dev/blachut/svelte/lang/utils.kt
@@ -3,10 +3,17 @@ package dev.blachut.svelte.lang
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.lang.PsiBuilder
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.TokenType
+import com.intellij.psi.search.FileTypeIndex
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.util.indexing.FileBasedIndexImpl
 import com.intellij.webSymbols.context.WebSymbolsContext
 import com.intellij.webSymbols.context.WebSymbolsContext.Companion.KIND_FRAMEWORK
 import com.intellij.xml.util.HtmlUtil
@@ -56,3 +63,14 @@ fun SvelteHtmlTag.isScriptOrStyleTag(): Boolean {
 internal inline fun <reified T : LocalInspectionTool> String.equalsName(): Boolean {
   return this == InspectionProfileEntry.getShortName(T::class.java.simpleName)
 }
+
+fun hasSvelteFiles(project: Project): Boolean =
+  CachedValuesManager.getManager(project).getCachedValue(project) {
+    CachedValueProvider.Result.create(
+      FileBasedIndexImpl.disableUpToDateCheckIn<Boolean, Exception> {
+        FileTypeIndex.containsFileOfType(SvelteHtmlFileType.INSTANCE, GlobalSearchScope.projectScope(project))
+      },
+      VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS,
+      DumbService.getInstance(project)
+    )
+  }
