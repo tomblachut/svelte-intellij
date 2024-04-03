d5cb69b700eebaa0357107938de1636f293fb5ba
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-04-03 16:02:08 +0200

    WEB-66321 Introduce hasFilesOfType utility
    GitOrigin-RevId: f20d0c362f46e0936e66ab3f92460b90ef6b53a9

--- src/main/java/dev/blachut/svelte/lang/utils.kt
+++ src/main/java/dev/blachut/svelte/lang/utils.kt
@@ -2,18 +2,12 @@ package dev.blachut.svelte.lang
 
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.javascript.web.hasFilesOfType
 import com.intellij.lang.PsiBuilder
-import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.TokenType
-import com.intellij.psi.search.FileTypeIndex
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.util.CachedValueProvider
-import com.intellij.psi.util.CachedValuesManager
-import com.intellij.util.indexing.FileBasedIndexImpl
 import com.intellij.webSymbols.context.WebSymbolsContext
 import com.intellij.webSymbols.context.WebSymbolsContext.Companion.KIND_FRAMEWORK
 import com.intellij.xml.util.HtmlUtil
@@ -65,13 +59,5 @@ internal inline fun <reified T : LocalInspectionTool> String.equalsName(): Boole
 }
 
 fun hasSvelteFiles(project: Project): Boolean {
-  return CachedValuesManager.getManager(project).getCachedValue(project) {
-    CachedValueProvider.Result.create(
-      FileBasedIndexImpl.disableUpToDateCheckIn<Boolean, Exception> {
-        FileTypeIndex.containsFileOfType(SvelteHtmlFileType, GlobalSearchScope.projectScope(project))
-      },
-      VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS,
-      DumbService.getInstance(project)
-    )
-  }
+  return hasFilesOfType(project, SvelteHtmlFileType)
 }
