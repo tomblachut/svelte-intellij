d7a0129847c8f3687227fe088acc65610eec7922
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-05-22 17:59:03 +0200

    [svelte] WEB-63964 add auto-popup test for store subscriptions
    GitOrigin-RevId: 2226bca90e65ce2578d5777a2f73ac248dea201d

--- src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteAutoPopupTest.kt
+++ src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteAutoPopupTest.kt
@@ -1,6 +1,8 @@
 package dev.blachut.svelte.lang.codeInsight
 
 import com.intellij.testFramework.fixtures.CompletionAutoPopupTestCase
+import dev.blachut.svelte.lang.SvelteTestScenario
+import dev.blachut.svelte.lang.doTestWithLangFromTestNameSuffix
 import junit.framework.TestCase
 
 class SvelteAutoPopupTest : CompletionAutoPopupTestCase() {
@@ -32,5 +34,23 @@ class SvelteAutoPopupTest : CompletionAutoPopupTestCase() {
     TestCase.assertNotNull(lookup)
     assertContainsElements(myFixture.lookupElementStrings!!, listOf("#if", "true"))
     assertDoesntContain(myFixture.lookupElementStrings!!, listOf("@html"))
+  }
+
+  fun testStoreSubscriptionJS() = doTestWithLangFromTestNameSuffix(storeSubscription)
+  fun testStoreSubscriptionTS() = doTestWithLangFromTestNameSuffix(storeSubscription)
+
+  private val storeSubscription = SvelteTestScenario { langExt, _ ->
+    myFixture.configureByText("Example.svelte", """
+      <script lang="$langExt">
+        import { writable } from 'svelte/store';
+
+        const count = writable(5);
+      </script>
+
+      <section>{<caret>}</section>
+    """.trimIndent())
+    type("$")
+    assertNotNull(lookup)
+    assertContainsElements(myFixture.lookupElementStrings!!, listOf("count"))
   }
 }
