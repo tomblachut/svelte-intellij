c973d9daa0098b54a1962efc2ece7f2e2ba1ea1a
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-05-22 02:45:46 +0200

    [svelte] reformat SvelteAutoPopupTest
    GitOrigin-RevId: d7e626ba8db5198ff5ab200c0489f1ad3049fd37

--- src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteAutoPopupTest.kt
+++ src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteAutoPopupTest.kt
@@ -4,33 +4,33 @@ import com.intellij.testFramework.fixtures.CompletionAutoPopupTestCase
 import junit.framework.TestCase
 
 class SvelteAutoPopupTest : CompletionAutoPopupTestCase() {
-    fun testAfterPrefix() {
-        myFixture.configureByText("Example.svelte", """<div on<caret>>""")
-        type(":")
-        TestCase.assertNotNull(lookup)
-        assertContainsElements(myFixture.lookupElementStrings!!, listOf("click"))
-    }
+  fun testAfterPrefix() {
+    myFixture.configureByText("Example.svelte", """<div on<caret>>""")
+    type(":")
+    TestCase.assertNotNull(lookup)
+    assertContainsElements(myFixture.lookupElementStrings!!, listOf("click"))
+  }
 
-    fun testOnModifiers() {
-        myFixture.configureByText("Example.svelte", """<div on:click<caret>>""")
-        type("|")
-        TestCase.assertNotNull(lookup)
-        type("p")
-        assertContainsElements(myFixture.lookupElementStrings!!, listOf("nonpassive", "stopPropagation"))
-    }
+  fun testOnModifiers() {
+    myFixture.configureByText("Example.svelte", """<div on:click<caret>>""")
+    type("|")
+    TestCase.assertNotNull(lookup)
+    type("p")
+    assertContainsElements(myFixture.lookupElementStrings!!, listOf("nonpassive", "stopPropagation"))
+  }
 
-    fun testSvelteExpression() {
-        myFixture.configureByText("Example.svelte", """text <caret>""")
-        type("{")
-        TestCase.assertNotNull(lookup)
-        assertContainsElements(myFixture.lookupElementStrings!!, listOf("#if", "@html", "true"))
-    }
+  fun testSvelteExpression() {
+    myFixture.configureByText("Example.svelte", """text <caret>""")
+    type("{")
+    TestCase.assertNotNull(lookup)
+    assertContainsElements(myFixture.lookupElementStrings!!, listOf("#if", "@html", "true"))
+  }
 
-    fun testSvelteExpression2() {
-        myFixture.configureByText("Example.svelte", """{<caret>}""")
-        type("#")
-        TestCase.assertNotNull(lookup)
-        assertContainsElements(myFixture.lookupElementStrings!!, listOf("#if", "true"))
-        assertDoesntContain(myFixture.lookupElementStrings!!, listOf("@html"))
-    }
+  fun testSvelteExpression2() {
+    myFixture.configureByText("Example.svelte", """{<caret>}""")
+    type("#")
+    TestCase.assertNotNull(lookup)
+    assertContainsElements(myFixture.lookupElementStrings!!, listOf("#if", "true"))
+    assertDoesntContain(myFixture.lookupElementStrings!!, listOf("@html"))
+  }
 }
