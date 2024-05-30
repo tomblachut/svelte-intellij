c081c20d08f489fd36c1a01b91120b5e0f2e684b
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-05-30 17:40:30 +0200

    [js] WEB-63964 introduce JSPatternBasedCompletionContributor.REFERENCE_EXPRESSION_PATTERN static field, use it in Svelte
    GitOrigin-RevId: 189a5d0560b99890890016336a74b3fe321b538a

--- src/main/java/dev/blachut/svelte/lang/completion/SvelteCompletionContributor.kt
+++ src/main/java/dev/blachut/svelte/lang/completion/SvelteCompletionContributor.kt
@@ -2,32 +2,17 @@ package dev.blachut.svelte.lang.completion
 
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.lang.ecmascript6.psi.ES6Property
 import com.intellij.lang.javascript.JSTokenTypes
-import com.intellij.lang.javascript.psi.JSReferenceExpression
+import com.intellij.lang.javascript.completion.JSPatternBasedCompletionContributor.REFERENCE_EXPRESSION_PATTERN
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PlatformPatterns.psiFile
-import com.intellij.patterns.PsiElementPattern
 import com.intellij.patterns.XmlPatterns
 import com.intellij.psi.xml.XmlTokenType
-import com.intellij.util.ProcessingContext
 import dev.blachut.svelte.lang.psi.SvelteHtmlFile
 import dev.blachut.svelte.lang.psi.SvelteJSLazyElementTypes
 
 class SvelteCompletionContributor : CompletionContributor() {
   init {
-    // from JSPatternBasedCompletionContributor
-    val referencePattern = psiElement(JSTokenTypes.IDENTIFIER)
-      .withParent(object : PsiElementPattern.Capture<JSReferenceExpression>(JSReferenceExpression::class.java) {
-        override fun accepts(o: Any?, context: ProcessingContext): Boolean {
-          if (!super.accepts(o, context)) return false
-          assert(o is JSReferenceExpression)
-          val parent = (o as JSReferenceExpression?)!!.parent
-          return !(parent is ES6Property && parent.isShorthanded)
-        }
-      })
-      .inFile(psiFile(SvelteHtmlFile::class.java))
-
     extend(
       CompletionType.BASIC,
       psiElement(XmlTokenType.XML_NAME).withParent(XmlPatterns.xmlAttribute()).inFile(psiFile(SvelteHtmlFile::class.java)),
@@ -46,7 +31,7 @@ class SvelteCompletionContributor : CompletionContributor() {
     )
     extend(
       CompletionType.BASIC,
-      referencePattern,
+      REFERENCE_EXPRESSION_PATTERN.inFile(psiFile(SvelteHtmlFile::class.java)),
       SvelteReferenceCompletionProvider()
     )
   }
