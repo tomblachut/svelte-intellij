99740acf76f6ce07f0179773e167c40ae07ac606
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-05-30 16:50:43 +0200

    [svelte] reformat SvelteTypeScriptHighlightingTest
    GitOrigin-RevId: cf21f9c0f22b4406be6173f41204903fbdb84620

--- src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteTypeScriptHighlightingTest.kt
+++ src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteTypeScriptHighlightingTest.kt
@@ -6,84 +6,78 @@ import dev.blachut.svelte.lang.getSvelteTestDataPath
 import junit.framework.TestCase
 
 class SvelteTypeScriptHighlightingTest : BasePlatformTestCase() {
-    override fun getTestDataPath(): String = getSvelteTestDataPath()
-    override fun getBasePath(): String = "dev/blachut/svelte/lang/codeInsight/tsHighlighting"
+  override fun getTestDataPath(): String = getSvelteTestDataPath()
+  override fun getBasePath(): String = "dev/blachut/svelte/lang/codeInsight/tsHighlighting"
 
-    override fun setUp() {
-        super.setUp()
-        myFixture.enableInspections(*SvelteHighlightingTest.configureDefaultLocalInspectionTools().toTypedArray())
-    }
+  override fun setUp() {
+    super.setUp()
+    myFixture.enableInspections(*SvelteHighlightingTest.configureDefaultLocalInspectionTools().toTypedArray())
+  }
 
-    fun testSimpleWithType() {
-        myFixture.configureByText(
-            "Usage.svelte",
-            """
-                <script lang="ts">
-                    export let testName: string;
-                    console.log(${"$$"}props);
-                    declare let test:number;
-                    if (test) {
-                        test = <error>"hello"</error>;
-                    } else {
-                        test = 2;
-                    }
-                    console.log(test);
-                </script>
-                <title>{testName}</title>
-                """.trimIndent()
-        )
-        myFixture.testHighlighting()
-    }
+  fun testSimpleWithType() {
+    myFixture.configureByText("Usage.svelte", """
+      <script lang="ts">
+        export let testName: string;
+        console.log(${"$$"}props);
+        declare let test:number;
+        if (test) {
+            test = <error>"hello"</error>;
+        } else {
+            test = 2;
+        }
+        console.log(test);
+      </script>
+      <title>{testName}</title>
+    """.trimIndent())
+    myFixture.testHighlighting()
+  }
 
-    fun testSimpleImport() {
-        myFixture.configureByText("hel.ts", "export const count = 1")
-        myFixture.configureByText(
-            "Usage.svelte",
-            """
-                <script lang="ts">
-                    import {count} from "./hel";
+  fun testSimpleImport() {
+    myFixture.configureByText("hello.ts", "export const count = 1")
+    myFixture.configureByText("Usage.svelte", """
+    <script lang="ts">
+      import {count} from "./hello";
 
-                    $: ${"$"}count && console.log('changed');
-                </script>
-                <title>test</title>
-                """.trimIndent()
-        )
-        myFixture.testHighlighting()
-    }
+      $: ${"$"}count && console.log('changed');
+    </script>
+    <title>test</title>
+    """.trimIndent())
+    myFixture.testHighlighting()
+  }
 
-    fun testTsconfigDirectAncestorDiscovery() {
-        val base = basePath + "/" + getTestName(true)
-        myFixture.configureByFiles("$base/src/App.svelte", "$base/tsconfig.json")
+  fun testTsconfigDirectAncestorDiscovery() {
+    val base = basePath + "/" + getTestName(true)
+    myFixture.configureByFiles("$base/src/App.svelte", "$base/tsconfig.json")
 
-        myFixture.testHighlighting()
-    }
+    myFixture.testHighlighting()
+  }
 
-    fun testTsconfigIndirectAncestorDiscovery() {
-        val base = basePath + "/" + getTestName(true)
-        myFixture.configureByFiles("$base/src/nested/Child.svelte", "$base/tsconfig.json")
+  fun testTsconfigIndirectAncestorDiscovery() {
+    val base = basePath + "/" + getTestName(true)
+    myFixture.configureByFiles("$base/src/nested/Child.svelte", "$base/tsconfig.json")
 
-        myFixture.testHighlighting()
-    }
+    myFixture.testHighlighting()
+  }
 
-    fun testSvelteKitPathMapping() {
-        myFixture.copyDirectoryToProject(basePath + "/" + getTestName(true), "")
-        myFixture.configureFromTempProjectFile("src/routes/+page.svelte")
-        myFixture.testHighlighting()
-        TestCase.assertNotNull(myFixture.findSingleIntention ("Insert 'import Counter from \"\$lib/Counter.svelte\"'"))
-    }
+  fun testSvelteKitPathMapping() {
+    myFixture.copyDirectoryToProject(basePath + "/" + getTestName(true), "")
+    myFixture.configureFromTempProjectFile("src/routes/+page.svelte")
+    myFixture.testHighlighting()
+    TestCase.assertNotNull(myFixture.findSingleIntention("Insert 'import Counter from \"\$lib/Counter.svelte\"'"))
+  }
 
-    fun testTsOverSvelte() {
-        myFixture.copyDirectoryToProject(basePath + "/" + getTestName(true), "")
-        myFixture.configureFromTempProjectFile("+page.svelte")
-        myFixture.testHighlighting()
-    }
+  fun testTsOverSvelte() {
+    myFixture.copyDirectoryToProject(basePath + "/" + getTestName(true), "")
+    myFixture.configureFromTempProjectFile("+page.svelte")
+    myFixture.testHighlighting()
+  }
 
   fun testViteAlias() {
     val root = myFixture.copyDirectoryToProject(basePath + "/" + getTestName(true), "")
     createAndSetViteConfig(project, testRootDisposable, "aliasPath", "src", null, root.path)
     myFixture.configureFromTempProjectFile(getTestName(false) + ".svelte")
     myFixture.testHighlighting()
-    TestCase.assertNotNull(myFixture.findSingleIntention ("Insert 'import Other from \"aliasPath/Other.svelte\"'"))
+    TestCase.assertNotNull(myFixture.findSingleIntention("Insert 'import Other from \"aliasPath/Other.svelte\"'"))
   }
 
 }
