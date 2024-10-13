8afb5c5f1a203ca66fb3e56e2e1d33c2c66a7ccb
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-10-13 22:03:36 +0200

    [ts] WEB-68605 colocate LspServerPackageDescriptors with JSServiceSetActivationRule
    GitOrigin-RevId: afa816802d8bc818cf20a2e1821d73cecb709a35

--- src/main/java/dev/blachut/svelte/lang/service/SvelteLspServerSupportProvider.kt
+++ src/main/java/dev/blachut/svelte/lang/service/SvelteLspServerSupportProvider.kt
@@ -1,27 +1,18 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package dev.blachut.svelte.lang.service
 
-import com.intellij.javascript.nodejs.util.NodePackageRef
 import com.intellij.lang.typescript.lsp.*
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
 import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
 import dev.blachut.svelte.lang.service.settings.SvelteServiceConfigurable
-import dev.blachut.svelte.lang.service.settings.getSvelteServiceSettings
 import icons.SvelteIcons
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification
 import org.eclipse.lsp4j.services.LanguageServer
-import org.jetbrains.annotations.ApiStatus
 
-private object SvelteLspServerPackageDescriptor : LspServerPackageDescriptor("svelte-language-server",
-                                                                             "0.17.1",
-                                                                             "/bin/server.js") {
-  override val defaultVersion: String get() = Registry.stringValue("svelte.language.server.default.version")
-}
 
 /**
  * @see SvelteLspTypeScriptService
@@ -47,11 +38,4 @@ class SvelteLspServerDescriptor(project: Project) : JSFrameworkLspServerDescript
 internal interface SvelteLsp4jServer : JSFrameworkLsp4jServer {
   @JsonNotification("\$/onDidChangeTsOrJsFile")
   fun didChangeTsOrJsFile(params: SvelteLspDidChangeTsOrJsFileParams)
-}
-
-@ApiStatus.Experimental
-object SvelteLspExecutableDownloader : LspServerDownloader(SvelteLspServerPackageDescriptor) {
-  override fun getSelectedPackageRef(project: Project): NodePackageRef {
-    return getSvelteServiceSettings(project).lspServerPackageRef
-  }
 }
--- src/main/java/dev/blachut/svelte/lang/service/SvelteServices.kt
+++ src/main/java/dev/blachut/svelte/lang/service/SvelteServices.kt
@@ -1,15 +1,47 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package dev.blachut.svelte.lang.service
 
+import com.intellij.javascript.nodejs.util.NodePackageRef
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptLanguageServiceUtil
 import com.intellij.lang.typescript.lsp.JSServiceSetActivationRule
+import com.intellij.lang.typescript.lsp.LspServerDownloader
+import com.intellij.lang.typescript.lsp.LspServerPackageDescriptor
+import com.intellij.lang.typescript.lsp.TSPluginDownloader
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import dev.blachut.svelte.lang.isSvelteContext
 import dev.blachut.svelte.lang.isSvelteProjectContext
 import dev.blachut.svelte.lang.service.settings.SvelteServiceMode
 import dev.blachut.svelte.lang.service.settings.getSvelteServiceSettings
+import org.jetbrains.annotations.ApiStatus
 
+
+private object SvelteLspServerPackageDescriptor : LspServerPackageDescriptor("svelte-language-server",
+                                                                             "0.17.1",
+                                                                             "/bin/server.js") {
+  override val defaultVersion: String get() = Registry.stringValue("svelte.language.server.default.version")
+}
+
+@ApiStatus.Experimental
+object SvelteLspExecutableDownloader : LspServerDownloader(SvelteLspServerPackageDescriptor) {
+  override fun getSelectedPackageRef(project: Project): NodePackageRef {
+    return getSvelteServiceSettings(project).lspServerPackageRef
+  }
+}
+
+private object SvelteTypeScriptPluginDescriptor : LspServerPackageDescriptor("typescript-svelte-plugin",
+                                                                             "0.3.42",
+                                                                             "") {
+  override val defaultVersion: String get() = Registry.stringValue("svelte.typescript.plugin.default.version")
+}
+
+@ApiStatus.Experimental
+object SvelteTypeScriptPluginPackageDownloader : TSPluginDownloader(SvelteTypeScriptPluginDescriptor) {
+  override fun getSelectedPackageRef(project: Project): NodePackageRef {
+    return getSvelteServiceSettings(project).tsPluginPackageRef
+  }
+}
 
 object SvelteServiceSetActivationRule : JSServiceSetActivationRule(SvelteLspExecutableDownloader, SvelteTypeScriptPluginPackageDownloader) {
   override fun isFileAcceptableForLspServer(file: VirtualFile): Boolean {
