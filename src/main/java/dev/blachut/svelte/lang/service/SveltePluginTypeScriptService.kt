8c491fe439891570dbd9ae8a3ae379289e11eea2
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-11-21 11:28:00 +0100

    WEB-70389 Webstorm keep freezing - get rid of long, blocking read actions on server init.
    GitOrigin-RevId: 2b513c5522ceffa44e97e1efb95346da721b665a

--- src/main/java/dev/blachut/svelte/lang/service/SveltePluginTypeScriptService.kt
+++ src/main/java/dev/blachut/svelte/lang/service/SveltePluginTypeScriptService.kt
@@ -1,6 +1,5 @@
 package dev.blachut.svelte.lang.service
 
-import com.intellij.lang.javascript.service.protocol.JSLanguageServiceObject
 import com.intellij.lang.javascript.service.protocol.JSLanguageServiceSimpleCommand
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptServiceWidgetItem
 import com.intellij.lang.typescript.compiler.languageService.frameworks.DownloadableTypeScriptServicePlugin
@@ -12,32 +11,26 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lang.lsWidget.LanguageServiceWidgetItem
 import dev.blachut.svelte.lang.service.settings.SvelteServiceConfigurable
 import icons.SvelteIcons
-import java.util.function.Consumer
 
 private val plugin = DownloadableTypeScriptServicePlugin("Svelte", SvelteServiceSetActivationRule)
 
 class SveltePluginTypeScriptService(project: Project) : PluggableTypeScriptService(project, plugin) {
   override fun getProcessName(): String =
-    // can't use instance fields here when legacy JSAsyncLanguageServiceBase.myToolWindowManager is not null
+  // can't use instance fields here when legacy JSAsyncLanguageServiceBase.myToolWindowManager is not null
     // and JSAsyncLanguageServiceBase.createDefaultReporter calls getProcessName() during <init>
     "Svelte + TypeScript" // todo replace with: "${plugin.shortLabel} + TypeScript"
 
-  override fun getInitialOpenCommands(): MutableMap<JSLanguageServiceSimpleCommand, Consumer<JSLanguageServiceObject>> {
-    val initialCommands = super.getInitialOpenCommands()
-    val result: MutableMap<JSLanguageServiceSimpleCommand, Consumer<JSLanguageServiceObject>> = linkedMapOf()
-    addConfigureCommand(result)
-
-    result.putAll(initialCommands)
-    return result
+  override fun getInitialOpenCommands(): List<JSLanguageServiceSimpleCommand> {
+    return listOf(createConfigureCommand()) + super.getInitialOpenCommands()
   }
 
-  private fun addConfigureCommand(result: MutableMap<JSLanguageServiceSimpleCommand, Consumer<JSLanguageServiceObject>>) {
+  private fun createConfigureCommand(): JSLanguageServiceSimpleCommand {
     // https://github.com/sveltejs/language-tools/pull/2185
     val arguments = ConfigurePluginRequestArguments(pluginName = "typescript-svelte-plugin",
                                                     configuration = mapOf("name" to "typescript-svelte-plugin",
                                                                           "enabled" to true,
                                                                           "assumeIsSvelteProject" to true))
-    result[ConfigurePluginRequest(arguments)] = Consumer {}
+    return ConfigurePluginRequest(arguments)
   }
 
   override fun isServiceFallbackResolveEnabled(): Boolean = true
