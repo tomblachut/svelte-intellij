1f9e17285728a7413bd5d24a2f309ecba05c345e
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-12-16 15:33:40 +0100

    [ts] WEB-68571 extract JSFrameworkLspServerSupportProvider to match JSFrameworkLspServerDescriptor
    GitOrigin-RevId: cf7ce702ac46a64f0b2380b79e1fb59f57155c91

--- src/main/java/dev/blachut/svelte/lang/service/SvelteLspServerSupportProvider.kt
+++ src/main/java/dev/blachut/svelte/lang/service/SvelteLspServerSupportProvider.kt
@@ -1,12 +1,13 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package dev.blachut.svelte.lang.service
 
-import com.intellij.lang.typescript.lsp.*
+import com.intellij.lang.typescript.lsp.JSFrameworkLsp4jServer
+import com.intellij.lang.typescript.lsp.JSFrameworkLspServerDescriptor
+import com.intellij.lang.typescript.lsp.JSFrameworkLspServerSupportProvider
+import com.intellij.lang.typescript.lsp.JSLspServerWidgetItem
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServer
-import com.intellij.platform.lsp.api.LspServerSupportProvider
-import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
 import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
 import dev.blachut.svelte.lang.service.settings.SvelteServiceConfigurable
 import icons.SvelteIcons
@@ -17,12 +18,8 @@ import org.eclipse.lsp4j.services.LanguageServer
 /**
  * @see SvelteLspTypeScriptService
  */
-class SvelteLspServerSupportProvider : LspServerSupportProvider {
-  override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerStarter) {
-    if (SvelteServiceSetActivationRule.isLspServerEnabledAndAvailable(project, file)) {
-      serverStarter.ensureServerStarted(SvelteLspServerDescriptor(project))
-    }
-  }
+class SvelteLspServerSupportProvider : JSFrameworkLspServerSupportProvider(SvelteServiceSetActivationRule) {
+  override fun createLspServerDescriptor(project: Project): JSFrameworkLspServerDescriptor = SvelteLspServerDescriptor(project)
 
   override fun createLspServerWidgetItem(lspServer: LspServer, currentFile: VirtualFile?): LspServerWidgetItem =
     JSLspServerWidgetItem(lspServer, currentFile, SvelteIcons.Original, SvelteIcons.Desaturated, SvelteServiceConfigurable::class.java)
