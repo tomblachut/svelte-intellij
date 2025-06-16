216084aa426c3ee5ea058fcb581434463455703b
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2025-06-16 15:15:53 +0200

    IJPL-189507 Move XML stub-related extensions to `intellij.xml.analysis.impl`
    GitOrigin-RevId: 4340fbdaade3b9f060f3a1c82951c8182ec0ce26

--- src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlFileElementType.kt
+++ src/main/java/dev/blachut/svelte/lang/parsing/html/SvelteHtmlFileElementType.kt
@@ -1,15 +1,15 @@
 package dev.blachut.svelte.lang.parsing.html
 
+import com.intellij.xml.HtmlLanguageStubVersionUtil
 import com.intellij.psi.stubs.PsiFileStub
 import com.intellij.psi.tree.IStubFileElementType
-import com.intellij.psi.xml.HtmlLanguageStubDefinition
 import dev.blachut.svelte.lang.SvelteHTMLLanguage
 import dev.blachut.svelte.lang.psi.SvelteJSElementTypes
 
 // based on HtmlFileElementType
 class SvelteHtmlFileElementType : IStubFileElementType<PsiFileStub<*>>("svelte file", SvelteHTMLLanguage.INSTANCE) {
   override fun getStubVersion(): Int {
-    return HtmlLanguageStubDefinition.getHtmlStubVersion() + SvelteJSElementTypes.STUB_VERSION
+    return HtmlLanguageStubVersionUtil.getHtmlStubVersion() + SvelteJSElementTypes.STUB_VERSION
   }
 
   companion object {
