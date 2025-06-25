e053c73de3fed38a05206e21c942c68408314ea2
Andrey Vorobev <andrey.vorobev@jetbrains.com>
2025-06-25 16:56:51 +0000

    [js] WEB-73585 ISE: CoroutinesKt.runBlockingCancellable - use only async approach under runBlocking and fix tests
    Merge-request: IJ-MR-166288
    Merged-by: Andrey Vorobev <Andrey.Vorobev@jetbrains.com>
    
    (cherry picked from commit 3ca98b45d862637064da863735225c3ea4e5be87)
    
    IJ-CR-165967
    
    GitOrigin-RevId: 8479fd2371b54268f979ff1db99e2f920a9e225e

--- src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteTypeScriptCompletionTest.kt
+++ src/test/java/dev/blachut/svelte/lang/codeInsight/SvelteTypeScriptCompletionTest.kt
@@ -1,25 +1,28 @@
 package dev.blachut.svelte.lang.codeInsight
 
+import com.intellij.lang.ecmascript6.psi.impl.JSImportsCoroutineScope
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.testFramework.utils.coroutines.waitCoroutinesBlocking
 
 class SvelteTypeScriptCompletionTest : BasePlatformTestCase() {
 
-    fun testTsImport() {
-        myFixture.configureByText("Hello.ts", "export class HelloTest {}")
-        myFixture.configureByText("Usage.svelte",
-            """
+  fun testTsImport() {
+    myFixture.configureByText("Hello.ts", "export class HelloTest {}")
+    myFixture.configureByText("Usage.svelte",
+                              """
                 <script lang="ts">
                     let z = new HelloT<caret>
                 </script>
                 """.trimIndent())
-        myFixture.completeBasic()
-        myFixture.checkResult(
-            """
+    myFixture.completeBasic()
+    waitCoroutinesBlocking(JSImportsCoroutineScope.get(project))
+    myFixture.checkResult(
+      """
                 <script lang="ts">
                     import {HelloTest} from "./Hello";
 
                     let z = new HelloTest()
                 </script>
                 """.trimIndent())
-    }
+  }
 }
