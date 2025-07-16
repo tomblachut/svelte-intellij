ab751949842f7adf4337e9734d75549685b3536e
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2025-07-16 21:46:20 +0200

    [lsp] IJPL-15740 support textDocument/foldingRange
    cherry-picked from be860524785ab2162d119d8a8a3f6061b60d9933
    
    IJ-CR-172189
    
    GitOrigin-RevId: a5a4adab853ebb3c00f6e509ef89c322dca18dc7

--- src/test/java/dev/blachut/svelte/lang/editor/SvelteEditorTest.kt
+++ src/test/java/dev/blachut/svelte/lang/editor/SvelteEditorTest.kt
@@ -1,77 +1,105 @@
 package dev.blachut.svelte.lang.editor
 
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilder
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.lang.folding.LanguageFolding
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.openapi.editor.Document
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import dev.blachut.svelte.lang.getSvelteTestDataPath
+import org.jetbrains.annotations.NotNull
 
 class SvelteEditorTest : BasePlatformTestCase() {
-    override fun getTestDataPath(): String = getSvelteTestDataPath()
-    override fun getBasePath(): String = "dev/blachut/svelte/lang/editor"
+  override fun getTestDataPath(): String = getSvelteTestDataPath()
+  override fun getBasePath(): String = "dev/blachut/svelte/lang/editor"
 
-    fun testEnterBetweenSvelteTags() {
-        myFixture.configureByText("Foo.svelte", "{#if test}<caret>{/if}")
-        myFixture.type('\n')
-        myFixture.checkResult(
-            """
-                {#if test}
-                    <caret>
-                {/if}
-                """.trimIndent()
-        )
+  private val fakeFoldingBuilder = object : FoldingBuilder {
+    override fun buildFoldRegions(@NotNull node: ASTNode, @NotNull document: Document): Array<FoldingDescriptor> {
+      return FoldingDescriptor.EMPTY_ARRAY
     }
 
-    fun testEnterBetweenHtmlTags() {
-        myFixture.configureByText("Foo.svelte", "<div><caret></div>")
-        myFixture.type('\n')
-        myFixture.checkResult(
-            """
-                <div>
-                    <caret>
-                </div>
-                """.trimIndent()
-        )
+    override fun getPlaceholderText(@NotNull node: ASTNode): String? {
+      return "..."
     }
 
-    fun testCompleteSvelteTag() {
-        myFixture.configureByText("Foo.svelte", "{#if test<caret>")
-        myFixture.type('}')
-        myFixture.checkResult("{#if test}<caret>{/if}")
+    override fun isCollapsedByDefault(@NotNull node: ASTNode): Boolean {
+      return false
     }
+  }
 
-    fun testCompleteSvelteTagWithSlash() {
-        myFixture.configureByText("Foo.svelte", "{#if test}hello{<caret>")
-        myFixture.type('/')
-        myFixture.checkResult("{#if test}hello{/if}<caret>")
-    }
+  fun testEnterBetweenSvelteTags() {
+    myFixture.configureByText("Foo.svelte", "{#if test}<caret>{/if}")
+    myFixture.type('\n')
+    myFixture.checkResult("""
+      {#if test}
+          <caret>
+      {/if}
+    """.trimIndent()
+    )
+  }
 
-    fun testCompleteSvelteTag3() {
-        myFixture.configureByText("Foo.svelte", """{#if true}{<caret>}""")
-        myFixture.type("/")
-        myFixture.checkResult("""{#if true}{/if}<caret>""")
-    }
+  fun testEnterBetweenHtmlTags() {
+    myFixture.configureByText("Foo.svelte", "<div><caret></div>")
+    myFixture.type('\n')
+    myFixture.checkResult("""
+      <div>
+          <caret>
+      </div>
+    """.trimIndent())
+  }
 
-    fun testCompleteSvelteTagAcrossClauses() {
-        myFixture.configureByText("Foo.svelte", """{#if true}{:else}{<caret>}""")
-        myFixture.type("/")
-        myFixture.checkResult("""{#if true}{:else}{/if}<caret>""")
-    }
+  fun testCompleteSvelteTag() {
+    myFixture.configureByText("Foo.svelte", "{#if test<caret>")
+    myFixture.type('}')
+    myFixture.checkResult("{#if test}<caret>{/if}")
+  }
 
-    fun testFoldingSvelteTag() {
-        doTestFolding()
-    }
+  fun testCompleteSvelteTagWithSlash() {
+    myFixture.configureByText("Foo.svelte", "{#if test}hello{<caret>")
+    myFixture.type('/')
+    myFixture.checkResult("{#if test}hello{/if}<caret>")
+  }
 
-    fun testFoldingEmptySvelteTag() {
-        doTestFolding()
-    }
+  fun testCompleteSvelteTag3() {
+    myFixture.configureByText("Foo.svelte", """{#if true}{<caret>}""")
+    myFixture.type("/")
+    myFixture.checkResult("""{#if true}{/if}<caret>""")
+  }
 
-    fun testFoldingSvelteTagInHtmlTag() {
-        doTestFolding()
-    }
+  fun testCompleteSvelteTagAcrossClauses() {
+    myFixture.configureByText("Foo.svelte", """{#if true}{:else}{<caret>}""")
+    myFixture.type("/")
+    myFixture.checkResult("""{#if true}{:else}{/if}<caret>""")
+  }
 
-    fun testFoldingEditorFold() {
-        doTestFolding()
-    }
+  fun testFoldingSvelteTag() {
+    doTestFolding()
+  }
 
-    private fun doTestFolding() {
-        myFixture.testFoldingWithCollapseStatus(testDataPath + "/" + basePath + "/" + getTestName(false) + ".svelte")
+  fun testFoldingEmptySvelteTag() {
+    doTestFolding()
+  }
+
+  fun testFoldingSvelteTagInHtmlTag() {
+    doTestFolding()
+  }
+
+  fun testFoldingEditorFold() {
+    try {
+      // some plugins contribute additional builders for XMLLanguage.INSTANCE,
+      // that leads to XML using CompositeFoldingBuilder, which we mock here;
+      // with the addition of LSP Folding support, Svelte started to also use CompositeFoldingBuilder,
+      // which actually fixes bugs around CompositeFoldingBuilder interop with multi-language trees.
+      LanguageFolding.INSTANCE.addExplicitExtension(XMLLanguage.INSTANCE, fakeFoldingBuilder)
+      doTestFolding()
     }
+    finally {
+      LanguageFolding.INSTANCE.removeExplicitExtension(XMLLanguage.INSTANCE, fakeFoldingBuilder)
+    }
+  }
+
+  private fun doTestFolding() {
+    myFixture.testFoldingWithCollapseStatus(testDataPath + "/" + basePath + "/" + getTestName(false) + ".svelte")
+  }
 }
