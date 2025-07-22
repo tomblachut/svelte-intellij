a6d2e725fe09a9fbc80531ffe1864a2bee94199b
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2025-07-22 18:37:04 +0200

    [svelte] include unexpected CompositeFoldingBuilder bug in actual SvelteEditorTest case
    GitOrigin-RevId: 7264909769a96f348717b8dd4d2d7ca3845d13de

--- src/test/java/dev/blachut/svelte/lang/editor/SvelteEditorTest.kt
+++ src/test/java/dev/blachut/svelte/lang/editor/SvelteEditorTest.kt
@@ -1,31 +1,56 @@
 package dev.blachut.svelte.lang.editor
 
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilder
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.lang.folding.LanguageFolding
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.openapi.editor.Document
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import dev.blachut.svelte.lang.SvelteHTMLLanguage
 import dev.blachut.svelte.lang.getSvelteTestDataPath
+import org.jetbrains.annotations.NotNull
 
 class SvelteEditorTest : BasePlatformTestCase() {
   override fun getTestDataPath(): String = getSvelteTestDataPath()
   override fun getBasePath(): String = "dev/blachut/svelte/lang/editor"
 
+  private val fakeFoldingBuilder = object : FoldingBuilder {
+    override fun buildFoldRegions(@NotNull node: ASTNode, @NotNull document: Document): Array<FoldingDescriptor> {
+      return FoldingDescriptor.EMPTY_ARRAY
+    }
+
+    override fun getPlaceholderText(@NotNull node: ASTNode): String? {
+      return "..."
+    }
+
+    override fun isCollapsedByDefault(@NotNull node: ASTNode): Boolean {
+      return false
+    }
+  }
+
   fun testEnterBetweenSvelteTags() {
     myFixture.configureByText("Foo.svelte", "{#if test}<caret>{/if}")
     myFixture.type('\n')
-    myFixture.checkResult("""
-      {#if test}
-          <caret>
-      {/if}
-    """.trimIndent()
+    myFixture.checkResult(
+      """
+                {#if test}
+                    <caret>
+                {/if}
+                """.trimIndent()
     )
   }
 
   fun testEnterBetweenHtmlTags() {
     myFixture.configureByText("Foo.svelte", "<div><caret></div>")
     myFixture.type('\n')
-    myFixture.checkResult("""
-      <div>
-          <caret>
-      </div>
-    """.trimIndent())
+    myFixture.checkResult(
+      """
+                <div>
+                    <caret>
+                </div>
+                """.trimIndent()
+    )
   }
 
   fun testCompleteSvelteTag() {
@@ -65,7 +90,16 @@ class SvelteEditorTest : BasePlatformTestCase() {
   }
 
   fun testFoldingEditorFold() {
-    doTestFolding()
+    try {
+      // some plugins contribute additional builders for XMLLanguage.INSTANCE,
+      // that leads to (buggy) usage of CompositeFoldingBuilder: XML uses CompositeFoldingBuilder, but Svelte does not
+      // let's encode this problem in tests
+      LanguageFolding.INSTANCE.addExplicitExtension(XMLLanguage.INSTANCE, fakeFoldingBuilder)
+      doTestFolding()
+    }
+    finally {
+      LanguageFolding.INSTANCE.removeExplicitExtension(XMLLanguage.INSTANCE, fakeFoldingBuilder)
+    }
   }
 
   private fun doTestFolding() {
