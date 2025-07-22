4426013af9166a6356a75ad57662528fec38a8fb
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2025-07-22 18:31:41 +0200

    [svelte] reformat SvelteEditorTest
    GitOrigin-RevId: 4b17fdc2dfbef6e01a6d3842b0022fbd2bbda12e

--- src/test/java/dev/blachut/svelte/lang/editor/SvelteEditorTest.kt
+++ src/test/java/dev/blachut/svelte/lang/editor/SvelteEditorTest.kt
@@ -4,74 +4,71 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import dev.blachut.svelte.lang.getSvelteTestDataPath
 
 class SvelteEditorTest : BasePlatformTestCase() {
-    override fun getTestDataPath(): String = getSvelteTestDataPath()
-    override fun getBasePath(): String = "dev/blachut/svelte/lang/editor"
+  override fun getTestDataPath(): String = getSvelteTestDataPath()
+  override fun getBasePath(): String = "dev/blachut/svelte/lang/editor"
 
-    fun testEnterBetweenSvelteTags() {
-        myFixture.configureByText("Foo.svelte", "{#if test}<caret>{/if}")
-        myFixture.type('\n')
-        myFixture.checkResult(
-            """
-                {#if test}
-                    <caret>
-                {/if}
-                """.trimIndent()
-        )
-    }
+  fun testEnterBetweenSvelteTags() {
+    myFixture.configureByText("Foo.svelte", "{#if test}<caret>{/if}")
+    myFixture.type('\n')
+    myFixture.checkResult("""
+      {#if test}
+          <caret>
+      {/if}
+    """.trimIndent()
+    )
+  }
 
-    fun testEnterBetweenHtmlTags() {
-        myFixture.configureByText("Foo.svelte", "<div><caret></div>")
-        myFixture.type('\n')
-        myFixture.checkResult(
-            """
-                <div>
-                    <caret>
-                </div>
-                """.trimIndent()
-        )
-    }
+  fun testEnterBetweenHtmlTags() {
+    myFixture.configureByText("Foo.svelte", "<div><caret></div>")
+    myFixture.type('\n')
+    myFixture.checkResult("""
+      <div>
+          <caret>
+      </div>
+    """.trimIndent())
+  }
 
-    fun testCompleteSvelteTag() {
-        myFixture.configureByText("Foo.svelte", "{#if test<caret>")
-        myFixture.type('}')
-        myFixture.checkResult("{#if test}<caret>{/if}")
-    }
+  fun testCompleteSvelteTag() {
+    myFixture.configureByText("Foo.svelte", "{#if test<caret>")
+    myFixture.type('}')
+    myFixture.checkResult("{#if test}<caret>{/if}")
+  }
 
-    fun testCompleteSvelteTagWithSlash() {
-        myFixture.configureByText("Foo.svelte", "{#if test}hello{<caret>")
-        myFixture.type('/')
-        myFixture.checkResult("{#if test}hello{/if}<caret>")
-    }
+  fun testCompleteSvelteTagWithSlash() {
+    myFixture.configureByText("Foo.svelte", "{#if test}hello{<caret>")
+    myFixture.type('/')
+    myFixture.checkResult("{#if test}hello{/if}<caret>")
+  }
 
-    fun testCompleteSvelteTag3() {
-        myFixture.configureByText("Foo.svelte", """{#if true}{<caret>}""")
-        myFixture.type("/")
-        myFixture.checkResult("""{#if true}{/if}<caret>""")
-    }
+  fun testCompleteSvelteTag3() {
+    myFixture.configureByText("Foo.svelte", """{#if true}{<caret>}""")
+    myFixture.type("/")
+    myFixture.checkResult("""{#if true}{/if}<caret>""")
+  }
 
-    fun testCompleteSvelteTagAcrossClauses() {
-        myFixture.configureByText("Foo.svelte", """{#if true}{:else}{<caret>}""")
-        myFixture.type("/")
-        myFixture.checkResult("""{#if true}{:else}{/if}<caret>""")
-    }
+  fun testCompleteSvelteTagAcrossClauses() {
+    myFixture.configureByText("Foo.svelte", """{#if true}{:else}{<caret>}""")
+    myFixture.type("/")
+    myFixture.checkResult("""{#if true}{:else}{/if}<caret>""")
+  }
 
-    fun testFoldingSvelteTag() {
-        doTestFolding()
-    }
+  fun testFoldingSvelteTag() {
+    doTestFolding()
+  }
 
-    fun testFoldingEmptySvelteTag() {
-        doTestFolding()
-    }
+  fun testFoldingEmptySvelteTag() {
+    doTestFolding()
+  }
 
-    fun testFoldingSvelteTagInHtmlTag() {
-        doTestFolding()
-    }
+  fun testFoldingSvelteTagInHtmlTag() {
+    doTestFolding()
+  }
 
-    fun testFoldingEditorFold() {
-        doTestFolding()
-    }
+  fun testFoldingEditorFold() {
+    doTestFolding()
+  }
 
-    private fun doTestFolding() {
-        myFixture.testFoldingWithCollapseStatus(testDataPath + "/" + basePath + "/" + getTestName(false) + ".svelte")
-    }
+  private fun doTestFolding() {
+    myFixture.testFoldingWithCollapseStatus(testDataPath + "/" + basePath + "/" + getTestName(false) + ".svelte")
+  }
 }
