24a10b5dba9925eb4a541f66efa26d23cc16248f
Arsenii Iusufov <arsenii.iusufov@jetbrains.com>
2026-01-23 15:48:35 +0100

    [svelte] WEB-74831 Cleanup `SvelteServiceSettings`
    Space-RevId: ad7079233960364a510b7699cef1ade65041d1bb
    
    GitOrigin-RevId: 691ed21514fc1478c582e859c7c28ef23162efce

--- src/main/java/dev/blachut/svelte/lang/service/settings/SvelteServiceSettings.kt
+++ src/main/java/dev/blachut/svelte/lang/service/settings/SvelteServiceSettings.kt
@@ -1,11 +1,8 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package dev.blachut.svelte.lang.service.settings
 
-import com.intellij.lang.typescript.lsp.NestedReadWriteProperty
-import com.intellij.lang.typescript.lsp.createPackageRef
-import com.intellij.lang.typescript.lsp.defaultPackageKey
-import com.intellij.lang.typescript.lsp.extractRefText
-import com.intellij.lang.typescript.lsp.restartTypeScriptServicesAsync
+import com.intellij.javascript.nodejs.util.NodePackageRef
+import com.intellij.lang.typescript.lsp.*
 import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import dev.blachut.svelte.lang.service.SvelteLspServerLoader
@@ -16,7 +13,7 @@ fun getSvelteServiceSettings(project: Project): SvelteServiceSettings = project.
 @Service(Service.Level.PROJECT)
 @State(name = "SvelteServiceSettings", storages = [Storage(StoragePathMacros.WORKSPACE_FILE)])
 class SvelteServiceSettings(val project: Project) : SimplePersistentStateComponent<SvelteServiceState>(SvelteServiceState()) {
-  var serviceMode
+  var serviceMode: SvelteServiceMode
     get() = state.innerServiceMode
     set(value) {
       val changed = state.innerServiceMode != value
@@ -24,7 +21,7 @@ class SvelteServiceSettings(val project: Project) : SimplePersistentStateCompone
       if (changed) restartTypeScriptServicesAsync(project)
     }
 
-  var lspServerPackageRef
+  var lspServerPackageRef: NodePackageRef
     get() = createPackageRef(state.lspServerPackageName, SvelteLspServerLoader.packageDescriptor.serverPackage)
     set(value) {
       val refText = extractRefText(value)
@@ -33,7 +30,7 @@ class SvelteServiceSettings(val project: Project) : SimplePersistentStateCompone
       if (changed) restartTypeScriptServicesAsync(project)
     }
 
-  var tsPluginPackageRef
+  var tsPluginPackageRef: NodePackageRef
     get() = createPackageRef(state.tsPluginPackageName, SvelteTSPluginLoader.packageDescriptor.serverPackage)
     set(value) {
       val refText = extractRefText(value)
@@ -42,15 +39,18 @@ class SvelteServiceSettings(val project: Project) : SimplePersistentStateCompone
       if (changed) restartTypeScriptServicesAsync(project)
     }
 
-  var showA11yWarnings by NestedReadWriteProperty({ state }, SvelteServiceState::showA11yWarnings)
+  var showA11yWarnings: Boolean by NestedReadWriteProperty(
+    currentReceiver = { state },
+    nestedProperty = SvelteServiceState::showA11yWarnings,
+  )
 }
 
 class SvelteServiceState : BaseState() {
-  var innerServiceMode by enum(SvelteServiceMode.ENABLED)
-  var lspServerPackageName by string(defaultPackageKey)
-  var tsPluginPackageName by string(defaultPackageKey)
+  var innerServiceMode: SvelteServiceMode by enum(SvelteServiceMode.ENABLED)
+  var lspServerPackageName: String? by string(defaultPackageKey)
+  var tsPluginPackageName: String? by string(defaultPackageKey)
 
-  var showA11yWarnings by property(true)
+  var showA11yWarnings: Boolean by property(true)
 }
 
 enum class SvelteServiceMode {
