8ab5ff9b365f1f14521dcaaba689497d40010c09
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2026-03-26 15:10:57 +0200

    WEB-77053 IJ-CR-199147 svelte: add SvelteTypeScriptServiceGetElementTypeTest to test suite
    (cherry picked from commit e8454c5cbe74b44e7b0550c5ea906cd6e9c686b0)
    
    GitOrigin-RevId: 2c861def5cf1898bc35419d40718398128f88506

--- src/test/java/dev/blachut/svelte/lang/SvelteTestSuite.kt
+++ src/test/java/dev/blachut/svelte/lang/SvelteTestSuite.kt
@@ -32,6 +32,7 @@ import dev.blachut.svelte.lang.service.SvelteNsUsagesServiceTest
 import dev.blachut.svelte.lang.service.SvelteServiceCompletionTest
 import dev.blachut.svelte.lang.service.SvelteServiceDocumentationTest
 import dev.blachut.svelte.lang.service.SvelteServiceTest
+import dev.blachut.svelte.lang.service.SvelteTypeScriptServiceGetElementTypeTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
@@ -78,6 +79,7 @@ class SvelteAggregatorTestSuite
   SvelteServiceDocumentationTest::class,
   SvelteNsUsagesServiceTest::class,
   SvelteNsRenameServiceTest::class,
+  SvelteTypeScriptServiceGetElementTypeTest::class,
 )
 class SvelteServiceTestSuite
 
