1475ae54665335de0b4a1144b583c4e19c48c5a9
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2026-03-30 11:35:05 +0300

    WEB-61817 svelte: refactor `SvelteNsRenameServiceTest`
    GitOrigin-RevId: 89b93c7247d36f3db93227635d2a8f3c22b1c186

---
+++ src/test/resources/dev/blachut/svelte/lang/service/nsRename/firstSegmentThreeLevel/Consumer.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+  import UI from './lib/UI';
+</script>
+<UI.Button.Label>Submit</UI.Button.Label>
---
+++ src/test/resources/dev/blachut/svelte/lang/service/nsRename/firstSegmentTwoLevel/Consumer.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+  import UI from './lib/UI';
+</script>
+<UI.Button>Click me</UI.Button>
